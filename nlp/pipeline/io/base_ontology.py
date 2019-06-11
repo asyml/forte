6934ae19bd343b0c90b9d33403b986fc40089a04
Wei Wei <wei.wei@petuum.com>
2019-06-11 14:49:20 -0400

    Add basic/ontonotes ontology and readers. Update ontonotes example (adding index and hiding object regeneration info).

--- nlp/pipeline/io/base_ontology.py
+++ nlp/pipeline/io/base_ontology.py
@@ -1,50 +1,37 @@
-""" This class defines the basic ontology of supported by our system
+""" This class defines the basic ontology supported by our system
 """
-from abc import ABCMeta, abstractmethod
-
-
-class Ontology:
-    """
-    The ontology class that is used to host all ontology definitions.
-    """
-    def __init__(self):
-        pass
-
-    def add_ontology_type(self, clz: type):
-        if not clz == TOP:
-            raise ValueError('Illegal ontology class type. All '
-                             'class types should be subclass of '
-                             'nlp.pipeline.io.base_ontology.TOP')
+from abc import abstractmethod
 
 
 class TOP:
-    def __init__(self, tid: str, component: str, is_gold: bool = False):
-        # tid is doc level universal id to identify this annotation, e.g.
-        # "entity_mention_12"
-        self.tid = tid
+    """The base class inherited by all annotations
 
-        # component is the engine name that creates this
-        # annotation, it should at least include the full engine name
-        # e.g. "nlp.pipeline.engines.ner", but can be more specific.
+    Args:
+        component (str): the engine name that creates this annotation,
+            it should at least include the full engine name.
+        tid (str, optional): a doc level universal id to identify
+            this annotation
+    """
+    def __init__(self, component: str, tid: str = None):
+        self.tid = f"{self.__class__.__qualname__}.{tid}"
         self.component = component
 
-        # this field is used to indicate whether it can be used as a gold
-        # standard. I am still thinking whether to use it.
-        self.is_gold = is_gold
+    def set_tid(self, tid: str):
+        self.tid = f"{self.__class__.__qualname__}.{tid}"
 
     @abstractmethod
     def hash(self):
         pass
 
     @abstractmethod
-    def eq(self):
+    def eq(self, other):
         pass
 
     def __hash__(self):
         return self.hash()
 
     def __eq__(self, other):
-        return self.eq()
+        return self.eq(other)
 
 
 class Span:
@@ -54,14 +41,32 @@ class Span:
 
 
 class Annotation(TOP):
-    def __init__(self, tid: str, component: str, span: Span):
-        super().__init__(tid, component)
+    def __init__(self, component: str, span: Span, tid: str = None):
+        super().__init__(component, tid)
         self.span = span
+
+    def hash(self):
+        return hash(
+            (
+                self.span.begin,
+                self.span.end,
+                self.component,
+                type(self)
+            )
+        )
+
+    def eq(self, other):
+        return (
+            (type(self) == type(other))
+            and (self.component == other.component)
+            and (self.span.begin == other.span.begin)
+            and (self.span.end == other.span.end)
+        )
 
 
 class Link(TOP):
-    def __init__(self, tid: str, component: str):
-        super().__init__(tid, component)
+    def __init__(self, component: str, tid: str = None):
+        super().__init__(component, tid)
         self.parent = None
         self.child = None
 
@@ -71,11 +76,89 @@ class Link(TOP):
     def set_child(self, child: TOP):
         self.child = child
 
+    def hash(self):
+        return hash(
+            (self.parent,
+             self.child,
+             self.component,
+             type(self))
+        )
+
+    def eq(self, other):
+        return (
+            (type(self) == type(other))
+            and (self.component == other.component)
+            and (self.parent == other.parent)
+            and (self.child == other.child)
+        )
+
 
 class Group(TOP):
-    def __init__(self, tid: str, component: str):
-        super().__init__(tid, component)
+    def __init__(self, component: str, tid: str = None):
+        super().__init__(component, tid)
         self.members = set()
 
     def add_member(self, member: TOP):
         self.members.add(member)
+
+    def eq(self, other):
+        return (
+            (type(self) == type(other))
+            and (self.members == other.members)
+        )
+
+
+class Meta:
+    def __init__(self, docid: str = None):
+        self.docid = docid
+
+
+class BaseOntology:
+    class Token(Annotation):
+        def __init__(self, component: str, span: Span, tid: str = None):
+            super().__init__(component, span, tid)
+
+    class Sentence(Annotation):
+        def __init__(self, component: str, span: Span, tid: str = None):
+            super().__init__(component, span, tid)
+
+    class EntityMention(Annotation):
+        def __init__(self, component: str, span: Span, tid: str = None):
+            super().__init__(component, span, tid)
+            self.ner_type = None
+
+    class PredicateArgument(Annotation):
+        def __init__(self,  component: str, span: Span, tid: str = None):
+            super().__init__(component, span, tid)
+
+    class PredicateLink(Link):
+        def __init__(self, component: str, tid: str = None):
+            super().__init__(component, tid)
+            self.arg_type = None
+
+        def set_argtype(self, arg_type: str):
+            self.arg_type = arg_type
+
+    class PredicateMention(Annotation):
+        def __init__(self, component: str, span: Span, tid: str = None):
+            super().__init__(component, span, tid)
+            self.links = []
+
+        def add_link(self, link: Link):
+            self.links.append(link)
+
+    class CoreferenceGroup(Group):
+        def __init__(self, component: str, tid: str = None):
+            super().__init__(component, tid)
+            self.coref_type = None
+
+        def eq(self, other):
+            return (
+                (type(self) == type(other))
+                and (self.coref_type == other.coref_type)
+                and (self.members == other.members)
+            )
+
+    class CoreferenceMention(Annotation):
+        def __init__(self, component: str, span: Span, tid: str = None):
+            super().__init__(component, span, tid)
--- nlp/pipeline/io/readers/ontonotes_reader.py
+++ nlp/pipeline/io/readers/ontonotes_reader.py
@@ -1,10 +1,316 @@
 """
-The reader that read ontonotes data into our internal json data format.
+The reader that reads Ontonotes data into our internal json data format.
 """
-
+import os
+import logging
+import codecs
+from typing import DefaultDict, List, Optional, Iterator, Tuple
+from collections import defaultdict
 from nlp.pipeline.io.readers.base_reader import BaseReader
+from nlp.pipeline.io.interchange import Interchange
+from nlp.pipeline.io.ontonotes_ontology import OntonotesOntology
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class OntonotesReader(BaseReader):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, lazy):
+        super().__init__(lazy)
+        self.ontology = OntonotesOntology
+
+    @staticmethod
+    def dataset_path_iterator(dir_path: str) -> Iterator[str]:
+        """
+        An iterator returning file_paths in a directory containing
+        CONLL-formatted files.
+        """
+        for root, _, files in os.walk(dir_path):
+            for data_file in files:
+                if data_file.endswith("gold_conll"):
+                    yield os.path.join(root, data_file)
+
+    def _read_document(self, file_path: str) -> Interchange:
+        doc = codecs.open(file_path, "r", encoding="utf8")
+
+        text = ""
+        offset = 0
+        has_rows = False
+
+        speaker = part_id = document_id = None
+        sentence_begin = 0
+
+        # auxiliary structures
+        current_entity_mention: Optional[Tuple[int, str]] = None
+        verbal_predicates: List[str] = []
+
+        current_pred_arg: List[Optional[Tuple[str, str]]] = []
+        verbal_pred_args: List[List[Tuple[str, str]]] = []
+
+        groups: DefaultDict[int, List[str]] = defaultdict(list)
+        coref_stacks: DefaultDict[int, List[int]] = defaultdict(list)
+
+        for line in doc:
+            line = line.strip()
+
+            if line.startswith("#end document"):
+                break
+
+            if line != "" and not line.startswith("#"):
+                conll_components = line.split()
+                document_id = conll_components[0]
+                part_id = int(conll_components[1])
+                word = conll_components[3]
+                pos_tag = conll_components[4]
+                lemmatised_word = conll_components[6]
+                framenet_id = conll_components[7]
+                word_sense = conll_components[8]
+                speaker = conll_components[9]
+                entity_label = conll_components[10]
+                pred_labels = conll_components[11:-1]
+
+                word_begin = offset
+                word_end = offset + len(word)
+
+                # add tokens
+                kwargs_i = {"pos_tag": pos_tag, "sense": word_sense}
+                self._add_span_annotation(
+                    self.ontology.Token, word_begin, word_end, **kwargs_i
+                )
+
+                # add entity mentions
+                current_entity_mention = self._process_entity_annotations(
+                    entity_label,
+                    word_begin,
+                    word_end,
+                    current_entity_mention
+                )
+
+                # add predicate mentions
+                if lemmatised_word != "-":
+                    word_is_verbal_predicate = any(
+                        ["(V" in x for x in pred_labels]
+                    )
+                    kwargs_i = {
+                        "framenet_id": framenet_id,
+                        "pred_lemma": lemmatised_word,
+                        "pred_type": "verb"
+                        if word_is_verbal_predicate
+                        else "other",
+                    }
+                    pred_mention = self._add_span_annotation(
+                        self.ontology.PredicateMention,
+                        word_begin,
+                        word_end,
+                        **kwargs_i
+                    )
+
+                    if word_is_verbal_predicate:
+                        verbal_predicates.append(pred_mention)
+
+                if not verbal_pred_args:
+                    current_pred_arg = [None for _ in pred_labels]
+                    verbal_pred_args = [[] for _ in pred_labels]
+
+                # add predicate arguments
+                self._process_pred_annotations(
+                    conll_components[11:-1],
+                    word_begin,
+                    word_end,
+                    current_pred_arg,
+                    verbal_pred_args,
+                )
+
+                # add coreference mentions
+                self._process_coref_annotations(
+                    conll_components[-1],
+                    word_begin,
+                    word_end,
+                    coref_stacks,
+                    groups,
+                )
+
+                text += word + " "
+                offset = word_end + 1
+                has_rows = True
+
+            else:
+                if not has_rows:
+                    continue
+
+                # add predicate links in the sentence
+                for predicate, pred_arg in zip(
+                    verbal_predicates, verbal_pred_args
+                ):
+                    for arg in pred_arg:
+                        kwargs_i = {"arg_type": arg[1]}
+                        link = self._add_link_annotation(
+                            self.ontology.PredicateLink,
+                            predicate,
+                            arg[0],
+                            **kwargs_i
+                        )
+
+                        self.current_interchange.index.annotation_index[
+                            predicate
+                        ].add_link(link)
+
+                verbal_predicates = []
+                current_pred_arg = []
+                verbal_pred_args = []
+
+                # add sentence
+                kwargs_i = {"speaker": speaker, "part_id": part_id}
+                self._add_span_annotation(
+                    self.ontology.Sentence,
+                    sentence_begin,
+                    offset - 1,
+                    **kwargs_i
+                )
+                sentence_begin = offset
+
+                has_rows = False
+
+        # group the coreference mentions in the whole document
+        for group_id, mention_list in groups.items():
+            kwargs_i = {"coref_type": group_id}
+            self._add_group_annotation(
+                self.ontology.CoreferenceGroup, mention_list, **kwargs_i
+            )
+
+        kwargs_i = {"docid": document_id}
+        self.current_interchange.set_meta(**kwargs_i)
+        self.current_interchange.text = text
+
+        doc.close()
+        return self.current_interchange
+
+    def _process_entity_annotations(
+        self,
+        label: str,
+        word_begin: int,
+        word_end: int,
+        current_entity_mention: Tuple[int, str],
+    ) -> Tuple[int, str]:
+
+        ner_type = label.strip("()*")
+
+        if "(" in label:
+            # Entering into a span for a particular ner.
+            current_entity_mention = (word_begin, ner_type)
+        if ")" in label:
+            # Exiting a span, add and then reset the current span.
+            kwargs_i = {"ner_type": current_entity_mention[1]}
+            self._add_span_annotation(
+                self.ontology.EntityMention,
+                current_entity_mention[0],
+                word_end,
+                **kwargs_i
+            )
+            current_entity_mention = None
+
+        return current_entity_mention
+
+    def _process_pred_annotations(
+        self,
+        labels: List[str],
+        word_begin: int,
+        word_end: int,
+        current_pred_arg: List[Optional[Tuple[int, str]]],
+        verbal_pred_args: List[List[Tuple[str, str]]],
+    ) -> None:
+
+        for label_index, label in enumerate(labels):
+
+            arg_type = label.strip("()*")
+            if arg_type == "V":
+                continue
+
+            if "(" in label:
+                # Entering into a span
+                current_pred_arg[label_index] = (word_begin, arg_type)
+            if ")" in label:
+                # Exiting a span
+                arg_begin = current_pred_arg[label_index][0]
+                arg_type = current_pred_arg[label_index][1]
+                pred_arg = self._add_span_annotation(
+                    self.ontology.PredicateArgument, arg_begin, word_end
+                )
+                verbal_pred_args[label_index].append((pred_arg, arg_type))
+                current_pred_arg[label_index] = None
+
+    def _process_coref_annotations(
+        self,
+        label: str,
+        word_begin: int,
+        word_end: int,
+        coref_stacks: DefaultDict[int, List[int]],
+        groups: DefaultDict[int, List[str]],
+    ) -> None:
+
+        if label == "-":
+            return
+        for segment in label.split("|"):
+            # The conll representation of coref spans allows spans to overlap.
+            if segment[0] == "(":
+                if segment[-1] == ")":
+                    # The span begins and ends at this word (single word span).
+                    group_id = int(segment[1:-1])
+                    coref_mention = self._add_span_annotation(
+                        self.ontology.CoreferenceMention, word_begin, word_end
+                    )
+                    groups[group_id].append(coref_mention)
+                else:
+                    # The span is starting, so we record the index of the word.
+                    group_id = int(segment[1:])
+                    coref_stacks[group_id].append(word_begin)
+            else:
+                # The span for this id is ending, but not start at this word.
+                group_id = int(segment[:-1])
+                start = coref_stacks[group_id].pop()
+                annotation = self._add_span_annotation(
+                    self.ontology.CoreferenceMention, start, word_end
+                )
+                groups[group_id].append(annotation)
+
+    def _record_fields(self):
+        self.current_interchange.add_fields(
+            ["speaker", "part_id", "span"],
+            self.component_name,
+            self.ontology.Sentence.__qualname__,
+        )
+        self.current_interchange.add_fields(
+            ["sense", "pos_tag", "span"],
+            self.component_name,
+            self.ontology.Token.__qualname__,
+        )
+        self.current_interchange.add_fields(
+            ["ner_type", "span"],
+            self.component_name,
+            self.ontology.EntityMention.__qualname__,
+        )
+        self.current_interchange.add_fields(
+            ["pred_lemma", "pred_type", "link", "span"],
+            self.component_name,
+            self.ontology.PredicateMention.__qualname__,
+        )
+        self.current_interchange.add_fields(
+            ["span"],
+            self.component_name,
+            self.ontology.PredicateArgument.__qualname__,
+        )
+        self.current_interchange.add_fields(
+            ["parent", "child", "arg_type"],
+            self.component_name,
+            self.ontology.PredicateLink.__qualname__,
+        )
+        self.current_interchange.add_fields(
+            ["span"],
+            self.component_name,
+            self.ontology.CoreferenceMention.__qualname__,
+        )
+        self.current_interchange.add_fields(
+            ["coref_type", "members"],
+            self.component_name,
+            self.ontology.CoreferenceGroup.__qualname__,
+        )
