c2af6f40ad4fded14bde1f6563700d5c5ca5c9c1
Wei Wei <wei.wei@petuum.com>
2019-08-05 11:29:36 -0400

    Fix CI errors

--- nlp/pipeline/base_pipeline.py
+++ nlp/pipeline/base_pipeline.py
@@ -1,8 +1,8 @@
 from abc import abstractmethod
 from typing import List, Dict, Iterator, Generic, Optional
-import yaml
 import logging
 
+import yaml
 from texar.torch import HParams
 
 from nlp.pipeline.utils import get_class
