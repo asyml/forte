67adf597d908e271d65db3e85776758c2cfd9556
hector.liu <hector.liu@petuum.com>
2019-08-09 11:42:45 -0400

    Add ProdigyReader

---
+++ nlp/pipeline/data/readers/prodigy_reader.py
@@ -0,0 +1,110 @@
+"""The reader that reads prodigy text data with annotations into Datapacks."""
+
+import json
+from typing import List
+from nlp.pipeline.data.ontology.base_ontology import Token, \
+    Document, EntityMention
+from nlp.pipeline.data.data_pack import DataPack
+from nlp.pipeline.data.readers.file_reader import MonoFileReader
+
+__all__ = [
+    "ProdigyReader"
+]
+
+
+class ProdigyReader(MonoFileReader):
+    """:class:`ProdigyTextReader` is designed to read in Prodigy output text
+    Args:
+        lazy (bool, optional): The reading strategy used when reading a
+            dataset containing multiple documents. If this is true,
+            ``iter()`` will return an object whose ``__iter__``
+            method reloads the dataset each time it's called. Otherwise,
+            ``iter()`` returns a list.
+    """
+
+    def __init__(self, lazy: bool = False):
+        super().__init__(lazy)
+        self.define_output_info()
+
+    def define_output_info(self):
+        self.output_info = {
+            Document: [],
+            Token: [],
+            EntityMention: ["ner_type"]
+        }
+
+    @staticmethod
+    def _iterable_json(file_path):
+        """
+        Iterates over the contents of a json file and yields
+        decoded lines.
+        :str file_path: path to the jsonl file
+        :yield: json dictionary
+        """
+        with open(file_path) as f:
+            for line in f:
+                yield json.loads(line)
+
+    @staticmethod
+    def dataset_path_iterator(file_path: str):
+        """
+        Yields a single document as json dictionary
+        :param file_path: path to the JSONL file
+        :yield: json dict
+        """
+        data_iterable = ProdigyReader._iterable_json(file_path)
+
+        for single_doc in data_iterable:
+            yield single_doc
+
+    def iter(self, file_path: str) -> List[DataPack]:
+        """
+        Reads a jsonl file into a list of DataPacks
+        TODO add another lazy function that gives an Iterator
+        :param dir_path:
+        :return: List[DataPack]
+        """
+        data_iterable = self._iterable_json(file_path)
+
+        DataPacks = []
+
+        for single_doc in data_iterable:
+            DataPacks.append(self.read(single_doc))
+
+        return DataPacks
+
+    def read(self, single_doc: dict) -> DataPack:  # type: ignore
+        """
+        Extracts the contents of a json dictionary into a Datapack
+        :param single_doc: json dictionary
+        :return: DataPack object
+        """
+        pack = DataPack()
+        text = single_doc['text']
+        tokens = single_doc['tokens']
+        spans = single_doc['spans']
+
+        document = Document(0, len(text))
+        pack.set_text(text)
+        pack.add_or_get_entry(document)
+
+        for token in tokens:
+            begin = token['start']
+            end = token['end']
+            token_entry = Token(begin, end)
+            pack.add_or_get_entry(token_entry)
+
+        for span_items in spans:
+            begin = span_items['start']
+            end = span_items['end']
+            annotation_entry = EntityMention(begin, end)
+            annotation_entry.ner_type = span_items['label']
+            pack.add_or_get_entry(annotation_entry)
+
+        pack.meta.doc_id = ""
+
+        return pack
+
+    def _read_document(self, file_path: str) -> DataPack:
+        # For some reason the mypy test needs this function to exist
+        raise NotImplementedError
--- nlp/pipeline/pipeline_test.py
+++ nlp/pipeline/pipeline_test.py
@@ -2,15 +2,17 @@
 Unit tests for Pipeline.
 """
 import unittest
+import json
+import tempfile
 from nlp.pipeline.pipeline import Pipeline
-from nlp.pipeline.data.readers import OntonotesReader
+from nlp.pipeline.data.readers import OntonotesReader, ProdigyReader
 from nlp.pipeline.processors.impl.dummy_processor import *
-from nlp.pipeline.data.ontology import relation_ontology
+from nlp.pipeline.processors.impl.dummy_pack_processor import DummyPackProcessor
+from nlp.pipeline.data.ontology import relation_ontology, base_ontology
 from nlp.pipeline.data.ontology.relation_ontology import *
 
 
 class PipelineTest(unittest.TestCase):
-
     def setUp(self) -> None:
         # Define and config the Pipeline
         self.dataset_path = "examples/ontonotes_sample_dataset/00"
@@ -40,6 +42,65 @@ class PipelineTest(unittest.TestCase):
                 tokens = [token.text for token in
                           pack.get_entries(Token, sentence)]
                 self.assertEqual(sent_text, " ".join(tokens))
+
+
+class ProdigyPipelineTest(unittest.TestCase):
+    def setUp(self) -> None:
+        # Define and config the Pipeline
+        self.fp = tempfile.NamedTemporaryFile(mode='w')
+        self.nlp = Pipeline(ontology=base_ontology)
+        self.nlp.set_reader(ProdigyReader())
+        self.processor = DummyPackProcessor()
+        self.nlp.add_processor(self.processor)
+        self.create_sample_file()
+
+    def tearDown(self) -> None:
+        self.fp.close()
+
+    def create_sample_file(self) -> None:
+
+        prodigy_entry = {
+            "text": "Lorem ipsum dolor sit amet",
+            "tokens": [{"text": "Lorem", "start": 0, "end": 5, "id": 0},
+                       {"text": "ipsum", "start": 6, "end": 11, "id": 1},
+                       {"text": "dolor", "start": 12, "end": 17, "id": 2},
+                       {"text": "sit", "start": 18, "end": 21, "id": 3},
+                       {"text": "amet", "start": 22, "end": 26, "id": 4}],
+            "spans": [{"start": 0, "end": 5, "token_start": 0, "token_end": 1,
+                       "label": "sample_latin"},
+                      {"start": 12, "end": 26, "token_start": 2, "token_end": 18,
+                       "label": "sample_latin"}],
+            "meta": {"id": "doc_1", "sect_id": 1, "version": "1"},
+            "_input_hash": 123456789,
+            "_task_hash": -123456789,
+            "_session_id": "abcd", "_view_id": "ner_manual", "answer": "accept"
+        }
+
+        # for entry in JSON_file:
+        json.dump(prodigy_entry, self.fp)
+        self.fp.write('\n')
+
+    def test_packs(self):
+        # get processed pack from dataset
+        for pack in self.nlp.process_dataset(self.fp.name):
+            # get documents from pack
+            for doc in pack.get_entries(base_ontology.Document):
+                self.token_check(doc, pack)
+                self.label_check(doc, pack)
+
+    def token_check(self, doc, pack):
+        doc_text = doc.text
+        # Compare document text with tokens
+        tokens = [token.text for token in
+                  pack.get_entries(base_ontology.Token, doc)]
+        self.assertEquals(tokens[2], "dolor")
+        self.assertEqual(doc_text.replace(" ", ""), "".join(tokens))
+
+    def label_check(self, doc, pack):
+        # make sure that the labels are read in correctly
+        labels = [label.ner_type for label in
+                  pack.get_entries(base_ontology.EntityMention, doc)]
+        self.assertEqual(labels, ["sample_latin", "sample_latin"])
 
 
 if __name__ == '__main__':
