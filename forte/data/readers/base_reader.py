0731ead08d4c50f9a269f04acbaa07f86b00ed4f
Sean Rosario <sean.drosario@petuum.com>
2019-08-19 17:31:55 -0400

    changed logic where iter is shared, and parse_pack and collect are abstract

--- forte/data/readers/base_reader.py
+++ forte/data/readers/base_reader.py
@@ -3,11 +3,13 @@ Base reader type to be inherited by all readers.
 """
 from abc import abstractmethod
 from pathlib import Path
-from typing import Iterator, Optional, Dict, Type, List, Union, Generic
+from typing import Iterator, Optional, Dict, Type, List, Union, Generic, Any
 
 import jsonpickle
 import logging
+import os
 
+from forte import config
 from forte.data.data_pack import DataPack
 from forte.data.multi_pack import MultiPack
 from forte.data.base_pack import PackType
@@ -26,12 +28,20 @@ class BaseReader(Generic[PackType]):
     """The basic data reader class.
     To be inherited by all data readers.
     """
-    def __init__(self, lazy: bool = True) -> None:
+    def __init__(self,
+                 lazy: bool = True,
+                 from_cache: bool = False,
+                 cache_file: Optional[Path] = None,
+                 append_to_cache: bool = False)-> None:
         self.lazy = lazy
+        self.from_cache = from_cache
         self._cache_directory: Optional[Path] = None
         self._ontology = base_ontology
         self.output_info: Dict[Type[Entry], Union[List, Dict]] = {}
         self.component_name = get_full_module_name(self)
+        self.cache_file = cache_file
+        self.append_to_cache = append_to_cache
+
 
     @property
     def pack_type(self):
@@ -59,36 +69,89 @@ class BaseReader(Generic[PackType]):
         """
         return jsonpickle.decode(string)
 
+    def collect(self, **kwargs) -> Iterator[Any]:
+        """
+        Gives an iterator of some object that can be converted to a DataPack
+        or indicate the cache key to read
+        TO BE IMPLEMENTED BY THE READER
+        :param: One of more data sources
+        :return Iterator[Any]: Implementation should yield a collection
+        """
+        raise NotImplementedError
+
+    def parse_pack(self, collection: Any) -> Iterator[PackType]:
+        """
+        Gives an iterator of Packs parsed from a collection
+        TO BE IMPLEMENTED BY THE READER
+        :param collection: Object that can be parsed into a Pack
+        :return Iterator[PackType]: Iterator of Packs
+        """
+        raise NotImplementedError
+
     @abstractmethod
-    def iter(self, **kwargs) -> Union[List[PackType], Iterator[PackType]]:
+    def _cache_key_function(collection):
+        # Computes the cache key based on the type of data
+        # To be implemented by the reader
+        raise NotImplementedError
+
+    # TODO:
+    def _get_cache_location(self, collection: Any) -> Path:
+        """
+        Gets the path to the cache file for a collection
+        :param collection: information to compute cache key
+        :return Path: file path to the cache file for a Pack
+        """
+        file_path = self._cache_key_function(collection)
+        return os.path.join(self._cache_directory, file_path)
+
+    def read_from_cache(self, cache_location) -> PackType:
+        """
+
+        :param cache_location: Path to the cache file
+        :return: Pack
+        """
+        logger.info("reading from cache file %s", cache_location)
+        return self._instances_from_cache_file(cache_location)
+
+
+    def iter(self, **kwargs):# -> Union[Iterator[PackType], List[PackType]]:
         """
         An iterator over the entire dataset, yielding all Packs processed.
         If not reading from cache, should call collect()
         """
-        cache_file = None
-        if self._cache_directory is not None:
-            cache_file = self._get_cache_location_for_file_path(dir_path)
-
-        has_cache = cache_file is not None and cache_file.exists()
-
-        if has_cache:
-            logger.info("reading from cache file %s", cache_file)
-            return List(self._instances_from_cache_file(cache_file))
-
-        logger.info("reading from original files in %s", dir_path)\
-
-
         if self.lazy:
-            for collection in self.collect(**kwargs):
-                yield self.parse_packs(collection)
-        else:
+            return self._lazy_iter(**kwargs)
 
+        else:
             datapacks: List[DataPack] = []
 
             for collection in self.collect(**kwargs):
-                datapacks.append(self.parse_packs(collection))
+                if self.from_cache:
+                    pack = self.read_from_cache(self._get_cache_location(collection))
+                else:
+                    pack = self.parse_pack(collection)
+
+                    # write to the cache if we need to.
+                    if self.cache_file:
+                        logger.info("Caching datapack to %s", self.cache_file)
+                        if self.append_to_cache:
+                            with self.cache_file.open('a') as cache:
+                                cache.write(self.serialize_instance(pack) + "\n")
+                        else:
+                            with self.cache_file.open('w') as cache:
+                                cache.write(self.serialize_instance(pack) + "\n")
+
+                    config.working_component = None
+                    datapacks.append(pack)
             return datapacks
 
+    def _lazy_iter(self, **kwargs):
+        for collection in self.collect(**kwargs):
+            if self.from_cache:
+                pack = self.read_from_cache(self._get_cache_location(collection))
+            else:
+                pack = self.parse_pack(collection)
+            yield pack
 
     def cache_data(self, cache_directory: str) -> None:
         """Specify the path to the cache directory.
@@ -105,21 +168,6 @@ class BaseReader(Generic[PackType]):
         self._cache_directory = Path(cache_directory)
         Path.mkdir(self._cache_directory, exist_ok=True)
 
-
-    @abstractmethod
-    def _cache_key_function(collection):
-        # Computes the cache key based on the type of data
-        # To be implemented by the reader
-        raise NotImplementedError
-
-    # TODO:
-    def _get_cache_location(self, collection):
-        """
-
-        :param collect: information to compute cache key
-        :return:
-        """
-        raise NotImplementedError
 
     def _get_cache_location_for_file_path(self,
                                           file_path: str) -> Optional[Path]:
