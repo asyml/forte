83e1e344755815938a82f8b50c2128f33e0139ca
Sean Rosario <sean.drosario@petuum.com>
2019-08-21 13:38:45 -0400

    added the stanfordnlp processor

---
+++ forte/data/ontology/stanfordnlp_ontology.py
@@ -0,0 +1,30 @@
+import forte.data.ontology.base_ontology as ontology
+from typing import List
+
+class Word(ontology.Token):
+    def __init__(self, begin: int, end: int):
+        super().__init__(begin, end)
+        self.dependency_relation = None
+        self.feats = None
+        self.governor = None
+        self.index = None
+        self.lemma = None
+        self.pos = None
+        self.text = None
+        self.upos = None
+        self.xpos = None
+        self.pos_tag = None
+
+class Token(ontology.Token):
+    def __init__(self, begin: int, end: int):
+        super().__init__(begin, end)
+        self.index = None
+        self.text = None
+        self.words = None
+
+class Sentence(ontology.Sentence):
+    def __init__(self):
+        super().__init__()
+        self.dependencies= None
+        self.tokens = None  # type: List[Token]
+        self.words = None  # type: List[Word]
---
+++ forte/processors/StanfordNLP_processor.py
@@ -0,0 +1,79 @@
+import stanfordnlp
+import forte.data.ontology.stanfordnlp_ontology as ontology
+from forte.processors.base import PackProcessor
+from forte.data import PackType
+from forte.common.resources import Resources
+
+__all__ = [
+    "StandfordNLPProcessor",
+]
+
+class StandfordNLPProcessor(PackProcessor):
+
+    def __init__(self):
+        super().__init__()
+        self.sentence_component = None
+        self._ontology = ontology
+        self.processors = ""
+
+    def define_input_info(self):
+        self.input_info = {
+            self._ontology.Document: ["span"]
+        }
+
+    def define_output_info(self):
+        self.output_info = {
+            self._ontology.Token: ["span"],
+            self._ontology.Sentence: ["span"]
+        }
+
+    def initialize(self, configs, resource: Resources):
+        self.processors = configs
+        self.nlp = stanfordnlp.Pipeline(self.processors)
+
+    def _process(self, input_pack: PackType):
+
+        text = input_pack.text
+        end_pos = 0
+
+        # sentence parsing
+        paragraphs = [p for p in text.split('\n') if p]
+        for paragraph in paragraphs:
+            sentences = self.nlp(paragraph).sentences
+            for sentence in sentences:
+                begin_pos = text.find(sentence.words[0], end_pos)
+                end_pos = text.find(sentence.words[-1], begin_pos) + len(sentence.words[-1])
+                sentence_entry = self._ontology.Sentence(begin_pos, end_pos)
+                input_pack.add_or_get_entry(sentence_entry)
+
+                if "tokenize" in self.processors:
+                    offset = sentence_entry.span.begin
+                    end_pos_word = 0
+                    for word in sentence.words:
+                        begin_pos_word = sentence.text.find(word.text, end_pos_word)
+                        end_pos_word = begin_pos + len(word.text)
+                        token = self._ontology.Token(
+                            begin_pos_word + offset, end_pos_word + offset
+                        )
+                        token.text = word.text
+
+                        if "pos" in self.processors:
+                            token.pos_tag = word.pos
+                            token.upos = word.upos
+
+                        if "lemma" in self.processors:
+                            token.lemma = word.lemma
+                            token.xpos = word.xpos
+
+                        if "depparse" in self.processors:
+                            token.dependency_relation = word.dependency_relation
+
+                        input_pack.add_or_get_entry(token)
+
+
+# processors = "tokenize,pos,lemma,depparse"
+# nlp = stanfordnlp.Pipeline(processors)
+# doc = nlp("This is a sentence")
+# sentences = doc.sentences
+# for token in sentences[0].tokens:
+
