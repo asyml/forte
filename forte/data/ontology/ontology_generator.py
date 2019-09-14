09240e85f9f7c395c96d76b21f5e9efeef066f06
Mansi Gupta <mgupta1410@gmail.com>
2019-09-13 22:09:37 -0400

    Fixed pylint errors, added type checks

--- forte/data/ontology/ontology_generator.py
+++ forte/data/ontology/ontology_generator.py
@@ -19,27 +19,26 @@ class OntologyGenerator:
     """
 
     # string constants
-    _ONTOLOGY_NAME = "ontology_name"
-    _IMPORTS = "imports"
-    _ENTRY_DEFINITIONS = "entry_definitions"
-    _ENTRY_NAME = "entry_name"
-    _PARENT_ENTRY = "parent_entry"
-    _ATTRIBUTES = "attributes"
-    _ATTRIBUTE_NAME = "attribute_name"
-    _ATTRIBUTE_TYPE = "attribute_type"
-    _ATTRIBUTE_DEFAULT_VALUE = "attribute_default_value"
-    _INDENT = ' ' * 4
+    _ONTOLOGY_NAME: str = "ontology_name"
+    _IMPORTS: str = "imports"
+    _ENTRY_DEFINITIONS: str = "entry_definitions"
+    _ENTRY_NAME: str = "entry_name"
+    _PARENT_ENTRY: str = "parent_entry"
+    _ATTRIBUTES: str = "attributes"
+    _ATTRIBUTE_NAME: str = "attribute_name"
+    _ATTRIBUTE_TYPE: str = "attribute_type"
+    _ATTRIBUTE_DEFAULT_VALUE: str = "attribute_default_value"
+    _INDENT: str = ' ' * 4
 
     # allowed parent entries and init arguments
-    _INIT_ARGUMENTS = {"forte.data.ontology.top.Annotation":
-                       "begin: int, end: int",
-
-                       "forte.data.ontology.top.Link":
-                       "parent: Optional[Entry] = None, "
-                       "child: Optional[Entry] = None",
-
-                       "forte.data.ontology.top.Group":
-                       "members: Optional[Set[Entry]] = None"}
+    _INIT_ARGUMENTS: Dict[str, str] = {
+        "forte.data.ontology.top.Annotation":
+            "begin: int, end: int",
+        "forte.data.ontology.top.Link":
+            "parent: Optional[Entry] = None, child: Optional[Entry] = None",
+        "forte.data.ontology.top.Group":
+            "members: Optional[Set[Entry]] = None"
+         }
 
     def __init__(self):
         # mapping from ontology full name to corresponding generated file and
@@ -49,7 +48,7 @@ class OntologyGenerator:
         # should be one of the keys of self._INIT_ARGUMENTS
         self._base_entry_map_seen: Dict[str, str] = {}
 
-    def generate_ontology(self, json_file_path):
+    def generate_ontology(self, json_file_path: str):
         """
         Function to generate and save the python ontology code after reading
         ontology from the input json file
@@ -64,41 +63,41 @@ class OntologyGenerator:
 
             # reading ontology
             json_string: str = f.read()
-            ontology: dict = json.loads(json_string)
+            ontology: Dict = json.loads(json_string)
             ontology_full_name: str = ontology[self._ONTOLOGY_NAME]
             imports: List[str] = ontology[self._IMPORTS]
-            entry_definitions: List[str] = ontology[self._ENTRY_DEFINITIONS]
+            entry_definitions: List[Dict] = ontology[self._ENTRY_DEFINITIONS]
 
             # creating ontology code
-            ontology_file_docstring = '"""\nOntology file for ' \
+            ontology_file_docstring: str = '"""\nOntology file for ' \
                                       'forte.data.ontology.example_ontology\n' \
                                       'Automatically generated file. ' \
                                       'Do not change by hand\n"""'
 
-            custom_imports = f"import typing\n"
+            custom_imports: str = f"import typing\n"
 
             ontology_name_code, ontology_folder, ontology_file_name \
-                = self.get_ontology_name_code(ontology_full_name)
+                = self._get_ontology_name_code(ontology_full_name)
 
-            ontology_code = f"{ontology_file_docstring}\n" \
-                            f"{custom_imports}" \
-                            f"{ontology_name_code}"
+            ontology_code: str = f"{ontology_file_docstring}\n" \
+                                 f"{custom_imports}" \
+                                 f"{ontology_name_code}"
 
             for import_module in imports:
                 ontology_code += f"import {import_module}\n"
 
             for entry_definition in entry_definitions:
-                ontology_code += self.get_entry_code(entry_definition)
+                ontology_code += self._get_entry_code(entry_definition)
 
             # creating ontology package directories in the current directory if
             # required
-            ontology_dir_path = os.path.join(os.getcwd(), ontology_folder)
+            ontology_dir_path: str = os.path.join(os.getcwd(), ontology_folder)
             if not os.path.isdir(ontology_dir_path):
                 os.makedirs(ontology_dir_path)
 
             # creating the ontology python file and populating it
-            ontology_file_path = os.path.join(ontology_dir_path,
-                                              ontology_file_name)
+            ontology_file_path: str = os.path.join(ontology_dir_path,
+                                                   ontology_file_name)
 
             with open(ontology_file_path, 'w') as file:
                 file.write(ontology_code)
@@ -109,7 +108,7 @@ class OntologyGenerator:
 
             return ontology_full_name, ontology_file_path, ontology_folder
 
-    def cleanup_generated_ontology(self, ontology_full_name):
+    def cleanup_generated_ontology(self, ontology_full_name: str):
         """
         Deletes the generated ontology file and corresponding folder if empty
         Args:
@@ -123,13 +122,15 @@ class OntologyGenerator:
             os.remove(ontology_file_path)
 
             # delete generated folders
-            top_path = os.path.join(os.getcwd(), ontology_folder.split('/')[0])
+            top_path: str = os.path.join(os.getcwd(),
+                                         ontology_folder.split('/')[0])
             all_files = [files for _, _, files in os.walk(top_path)]
             num_files = sum([len(files) for files in all_files])
             if num_files == 0:
                 shutil.rmtree(top_path)
 
-    def get_ontology_name_code(self, ontology_full_name):
+    @staticmethod
+    def _get_ontology_name_code(ontology_full_name: str):
         """
         Function to parse ontology package and name, create required folders
         if the ontology package is not the same as current package, and generate
@@ -142,16 +143,16 @@ class OntologyGenerator:
             - Directory path where the generated file would be stored
             - Name of the file to be generated
         """
-        namespace = ontology_full_name.split('.')
-        ontology_package = '.'.join(namespace[0:-1])
-        code_str = f"import {ontology_package}\n"
+        namespace: List[str] = ontology_full_name.split('.')
+        ontology_package: str = '.'.join(namespace[0:-1])
+        code_str: str = f"import {ontology_package}\n"
 
-        ontology_folder = os.path.join(*namespace[0:-1])
-        ontology_file_name = namespace[-1] + '.py'
+        ontology_folder: str = os.path.join(*namespace[0:-1])
+        ontology_file_name: str = namespace[-1] + '.py'
 
         return code_str, ontology_folder, ontology_file_name
 
-    def get_entry_code(self, entry_definition):
+    def _get_entry_code(self, entry_definition: Dict):
         """
         Args:
             entry_definition: entry definition dictionary
@@ -159,38 +160,39 @@ class OntologyGenerator:
         Returns: code generated by an entry definition
         """
 
-        entry_full_name = entry_definition[self._ENTRY_NAME]
-        entry_name = entry_full_name.split('.')[-1]
-        parent_entry = entry_definition[self._PARENT_ENTRY]
+        entry_full_name: str = entry_definition[self._ENTRY_NAME]
+        entry_name: str = entry_full_name.split('.')[-1]
+        parent_entry: str = entry_definition[self._PARENT_ENTRY]
 
-        attributes = entry_definition[self._ATTRIBUTES] \
+        attributes: List[Dict] = entry_definition[self._ATTRIBUTES] \
             if self._ATTRIBUTES in entry_definition else []
 
-        base_entry = self.get_and_set_base_entry(entry_full_name, parent_entry)
-        init_arguments = self._INIT_ARGUMENTS[base_entry]
-        super_arguments = ', '.join([item.split(':')[0].strip()
-                                     for item in init_arguments.split(',')])
+        base_entry: str = self._get_and_set_base_entry(entry_full_name,
+                                                       parent_entry)
+        init_arguments: str = self._INIT_ARGUMENTS[base_entry]
+        super_arguments: str = ', '.join(
+            [item.split(':')[0].strip()for item in init_arguments.split(',')])
 
-        class_line = f"class {entry_name}({parent_entry}):"
-        init_line = f"def __init__(self, {init_arguments}):"
-        super_line = f"super().__init__({super_arguments})\n"
+        class_line: str = f"class {entry_name}({parent_entry}):"
+        init_line: str = f"def __init__(self, {init_arguments}):"
+        super_line: str = f"super().__init__({super_arguments})\n"
 
-        entry_code = f"\n\n{class_line}\n{self._INDENT}{init_line}\n" \
+        entry_code: str = f"\n\n{class_line}\n{self._INDENT}{init_line}\n"\
             f"{self._INDENT}{self._INDENT}{super_line}"
 
         for attribute in attributes:
-            attribute_init_code = self.get_attribute_init_code(attribute)
+            attribute_init_code: str = self._get_attribute_init_code(attribute)
             entry_code += f"{attribute_init_code}"
 
         for attribute in attributes:
             entry_code += '\n'
-            attribute_getter_setter_code = \
-                self.get_attribute_getter_setter_code(attribute)
+            attribute_getter_setter_code: str = \
+                self._get_attribute_getter_setter_code(attribute)
             entry_code += f"{attribute_getter_setter_code}"
 
         return entry_code
 
-    def get_and_set_base_entry(self, entry_name, parent_entry):
+    def _get_and_set_base_entry(self, entry_name: str, parent_entry: str):
         """
         Function to return `base_entry` which is the entry on which the
          arguments would be based and populates `self._base_entry_map_seen`
@@ -212,11 +214,11 @@ class OntologyGenerator:
         if parent_entry in self._INIT_ARGUMENTS:
             return parent_entry
         else:
-            base_entry = self._base_entry_map_seen[parent_entry]
+            base_entry: str = self._base_entry_map_seen[parent_entry]
         self._base_entry_map_seen[entry_name] = base_entry
         return base_entry
 
-    def get_attribute_init_code(self, attribute):
+    def _get_attribute_init_code(self, attribute: Dict):
         """
         Args:
             attribute: Dictionary corresponding to a single attribute
@@ -225,23 +227,24 @@ class OntologyGenerator:
         Returns: code generated by an attribute in the self.__init__ function
 
         """
-        attribute_name = attribute[self._ATTRIBUTE_NAME]
-        attribute_type = attribute[self._ATTRIBUTE_TYPE]
+        attribute_name: str = attribute[self._ATTRIBUTE_NAME]
+        attribute_type: str = attribute[self._ATTRIBUTE_TYPE]
 
         if self._ATTRIBUTE_DEFAULT_VALUE in attribute and \
                 attribute[self._ATTRIBUTE_DEFAULT_VALUE] is not None:
-            attribute_default_value = attribute[self._ATTRIBUTE_DEFAULT_VALUE]
+            attribute_default: str = attribute[self._ATTRIBUTE_DEFAULT_VALUE]
             if attribute_type == "str":
-                attribute_default_value = f"'{attribute_default_value}'"
+                attribute_default = f"'{attribute_default}'"
         else:
-            attribute_default_value = "None"
+            attribute_default = "None"
 
-        code_string = f"{self._INDENT}{self._INDENT}self._{attribute_name}: " \
-            f"typing.Optional[{attribute_type}] = {attribute_default_value}\n"
+        code_string: str = f"{self._INDENT}{self._INDENT}" \
+            f"self._{attribute_name}: typing.Optional[{attribute_type}] " \
+            f"= {attribute_default}\n"
 
         return code_string
 
-    def get_attribute_getter_setter_code(self, attribute):
+    def _get_attribute_getter_setter_code(self, attribute: Dict):
         """
 
         Args:
@@ -251,8 +254,9 @@ class OntologyGenerator:
         Returns: getter and setter functions generated by an attribute
 
         """
-        attribute_name = attribute[self._ATTRIBUTE_NAME]
-        attribute_type = attribute[self._ATTRIBUTE_TYPE]
+        attribute_name: str = attribute[self._ATTRIBUTE_NAME]
+        attribute_type: str = attribute[self._ATTRIBUTE_TYPE]
+
         return f"{self._INDENT}@property\n" \
             f"{self._INDENT}def {attribute_name}(self):\n" \
             f"{self._INDENT}{self._INDENT}return self._{attribute_name}\n\n" \
