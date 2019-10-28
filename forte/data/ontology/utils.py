708706b46fba72f6070d50418a71785701171c35
Mansi Gupta <mgupta1410@gmail.com>
2019-10-28 15:14:08 -0400

    Pylint changes

--- forte/data/ontology/utils.py
+++ forte/data/ontology/utils.py
@@ -6,11 +6,6 @@ from importlib import util as import_util
 from pathlib import Path
 from pydoc import locate
 
-from typing import List, Dict, Union, Type
-
-from forte.data.base_pack import PackType
-from forte.data.ontology.core import Entry
-
 
 def get_user_objects_from_module(module_str, custom_dirs=None):
     """
@@ -39,34 +34,6 @@ def get_user_objects_from_module(module_str, custom_dirs=None):
                 return module.__all__
         return []
     return module.__all__
-
-
-def record_fields(output_info: Dict[Type[Entry], Union[List, Dict]],
-                  component_name: str, input_pack: PackType):
-    """
-    Record the fields and entries that this processor add to packs.
-
-    Args:
-        component_name: The component name that do the processing. This will be
-        record here to remember which processor (or reader) alter the entries.
-
-        output_info: The output information as specified in a reader of
-        process. It contains the information of which types or fields are
-        created or altered.
-
-        input_pack: The data pack where the information will be written on.
-
-    Returns:
-    """
-    for entry_type, info in output_info.items():
-        fields: List[str] = []
-        if isinstance(info, list):
-            fields = info
-        elif isinstance(info, dict):
-            fields = info["fields"]
-            if "component" in info.keys():
-                component_name = info["component"]
-        input_pack.record_fields(fields, entry_type, component_name)
 
 
 def search_in_dirs(file, dirs_paths):
