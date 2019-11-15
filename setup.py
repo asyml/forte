7b755f5c270f42915146a05d50565c3bc4825049
Avinash <avinashnb141@gmail.com>
2019-11-15 15:28:38 -0500

    Add Indexer into Forte (#78)
    * A basic wikipedia ontology
    
    * A basic wikipedia ontology
    
    * A basic setup for the Wiki parser based on the mwlinks.
    
    * A couple design fixes
    
    * Wikipedia parser
    
    * Deailing with the bug of processing duplications.
    
    * Smoothing the processing pipeline
    
    * Adding wiki writer.
    
    * Cleaning the batch processor.
    
    * Fixing batcher processor interfaces.
    
    * Abstracting the Multipack.
    
    * batch updates
    
    * We should not update the full index every time.
    
    * Fixing typing issues.
    
    * Fixing typing issue and batcher.
    
    * Fixing multipack and batcher interactions
    
    * Fixing multipack and batcher interactions
    
    * fix selector typing
    
    * make sure things are done within the pipeline
    
    * make sure the examples follow the correct convention
    
    * ensure depenencies in exampels
    
    * cannot solve the class entry type problem, currently ignoring the type error from mypy
    
    * optimize imports
    
    * Fixing the typing and cyclic import issues.
    
    * Draft version of indexer
    
    * Add retrieval-based chatbot example
    
    * Fix pylint
    
    * Update travis to include indexer libraries
    
    * Remove faiss from setup.py. Remove dependency on Pytorch URL wheel file.
    
    * Add tests and update docs for indexers
    
    * Update logic of indexer to handle GPU/CPU logic efficiently
    
    * Add faiss library as an extra
    
    * Add a Query and Embedding type
    
    * Add copyright header
    
    * Add save and load test case for indexer. Use setUpClass and tearDownClass instead of setUp and tearDown
    
    * Add selector test cases
    
    * Add codecov yml
    
    * Use coverage with pytest instead of unittest
    
    * Remove _define_output_info and _define_input_info from Non-batch processors
    
    * Add copyright headers

--- setup.py
+++ setup.py
@@ -32,7 +32,8 @@ setuptools.setup(
         'stanfordnlp': ['stanfordnlp'],
         'test': ['ddt'],
         'example': ['termcolor'],
-        'wikipedia': ['rdflib']
+        'wikipedia': ['rdflib'],
+        'indexer': ['faiss>=1.5.3']
     },
     package_data={
     },
