81964c8b6a028c910b51f72deaa7508fba1b8573
Swapnil Singhavi <swapnil.singhavi@petuum.com>
2020-01-16 14:49:15 -0500

    fix linting

--- forte/processors/spacy_processors.py
+++ forte/processors/spacy_processors.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import List
-
 import spacy
 from spacy.language import Language
 from texar.torch import HParams
