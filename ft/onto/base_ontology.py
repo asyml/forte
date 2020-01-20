6d752cecd586d937b6a29ec57b788223f47b3168
hector.liu <hector.liu@petuum.com>
2020-01-20 17:55:19 -0500

    Add generated ontologies.

---
+++ ft/onto/base_ontology.py
@@ -0,0 +1,261 @@
+# Copyright 2019 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+This class defines the basic ontology supported by our system
+"""
+from typing import Optional, Dict, List, Set, Type
+from forte.data.data_pack import DataPack
+from forte.data.ontology import Entry, Annotation, Link, Group
+
+__all__ = [
+    "Token",
+    "Sentence",
+    "Document",
+    "EntityMention",
+    "PredicateArgument",
+    "PredicateMention",
+    "PredicateLink",
+    "CoreferenceGroup",
+    "Dependency",
+    "RelationLink"
+]
+
+
+class Token(Annotation):
+    """
+    A span based annotation :class:`Token`.
+
+    Args:
+        pack (DataPack): The data pack this token belongs to.
+        begin (int): The offset of the first character in the token.
+        end (int): The offset of the last character in the token + 1.
+
+    Attributes:
+        ud_xpos (str): Language specific pos tag. Used in CoNLL-U Format. Refer
+        https://universaldependencies.org/format.html
+        lemma (str): Lemma or stem of word form.
+        is_root (bool): If the token is a root of, say, dependency tree.
+        ud_misc (Dict[str, List[str]]): Miscellaneous ud_features. Used in
+        CoNLL-U Format. Refer https://universaldependencies.org/format.html
+    """
+
+    def __init__(self, pack: DataPack, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.pos: Optional[str] = None
+        self.ud_xpos: Optional[str] = None
+        self.lemma: Optional[str] = None
+        self.chunk: Optional[str] = None
+        self.ner: Optional[str] = None
+        self.sense: Optional[str] = None
+        self.is_root: Optional[bool] = None
+        self.ud_features: Optional[Dict[str, List[str]]] = None
+        self.ud_misc: Optional[Dict[str, List[str]]] = None
+
+
+class Sentence(Annotation):
+    """
+    A span based annotation :class:`Sentence`.
+
+    Args:
+        pack (DataPack): The data pack this token belongs to.
+        begin (int): The offset of the first character in the sentence.
+        end (int): The offset of the last character in the sentence + 1.
+    """
+
+    def __init__(self, pack: DataPack, begin: int, end: int):
+        super().__init__(pack, begin, end)
+
+
+class Document(Annotation):
+    """
+    A span based annotation :class:`Document`.
+
+    Args:
+        pack (DataPack): The data pack this token belongs to.
+        begin (int): The offset of the first character in the document.
+        end (int): The offset of the last character in the document + 1.
+    """
+
+    def __init__(self, pack: DataPack, begin: int, end: int):
+        super().__init__(pack, begin, end)
+
+
+class EntityMention(Annotation):
+    """
+    A span based annotation :class:`EntityMention`.
+
+    Args:
+        pack (DataPack): The data pack this token belongs to.
+        begin (int): The offset of the first character in the entity mention.
+        end (int): The offset of the last character in the entity mention + 1.
+    """
+
+    def __init__(self, pack: DataPack, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.ner_type: Optional[str] = None
+
+
+class PredicateArgument(Annotation):
+    """
+    A span based annotation :class:`PredicateArgument`.
+
+    Args:
+        pack (DataPack): The data pack this token belongs to.
+        begin (int): The offset of the first character in the predicate
+            argument.
+        end (int): The offset of the last character in the predicate argument
+            + 1.
+    """
+
+    def __init__(self, pack: DataPack, begin: int, end: int):
+        super().__init__(pack, begin, end)
+
+
+class PredicateMention(Annotation):
+    """
+    A span based annotation :class:`PredicateMention`.
+
+    Args:
+        begin (int): The offset of the first character in the predicate mention.
+        end (int): The offset of the last character in the predicate mention
+            + 1.
+    """
+
+    def __init__(self, pack: DataPack, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.predicate_lemma: str
+        self.framenet_id: str
+        self.is_verb: bool
+
+
+class PredicateLink(Link):
+    """
+    A :class:`Link` type entry which take :class:`PredicateMention` as parent
+    and :class:`PredicateArgument` as child.
+
+    Args:
+        parent (Entry, optional): the parent entry of the link.
+        child (Entry, optional): the child entry of the link.
+    """
+
+    ParentType = PredicateMention
+    """The entry type of the parent node of :class:`PredicateLink`."""
+
+    ChildType = PredicateArgument
+    """The entry type of the child node of :class:`PredicateLink`."""
+
+    def __init__(self,
+                 pack: DataPack,
+                 parent: Optional[PredicateMention] = None,
+                 child: Optional[PredicateArgument] = None):
+        super().__init__(pack, parent, child)
+        self.arg_type = None
+
+
+# pylint: disable=duplicate-bases
+class CoreferenceGroup(Group):
+    """
+    A :class:`Group` type entry which take :class:`EntityMention` as
+    members.
+
+    Args:
+        members (Set[EntityMention], optional): a set of
+            :class:`EntityMention` objects which are the members of the
+            group.
+    """
+    MemberType = EntityMention
+    """The entry type of group members of :class:`CoreferenceGroup`."""
+
+    def __init__(
+            self,
+            pack: DataPack,
+            members: Optional[Set[Entry]] = None
+    ):
+        super().__init__(pack, members)
+
+
+class Dependency(Link):
+    """
+    A :class:`Link` type entry which represent a syntactic dependency.
+    """
+    ParentType = Token
+    """The entry type of the parent node of :class:`Dependency`."""
+
+    ChildType = Token
+    """The entry type of the child node of :class:`Dependency`."""
+
+    def __init__(self,
+                 pack: DataPack,
+                 parent: Optional[Token] = None,
+                 child: Optional[Token] = None):
+        super().__init__(pack, parent, child)
+        self.dep_label: Optional[str] = None
+
+
+class EnhancedDependency(Link):
+    """
+    A :class:`Link` type entry which represent a enhanced dependency :
+    https://universaldependencies.org/u/overview/enhanced-syntax.html
+    """
+    ParentType = Token
+    """The entry type of the parent node of :class:`Dependency`."""
+
+    ChildType = Token
+    """The entry type of the child node of :class:`Dependency`."""
+
+    def __init__(self,
+                 pack: DataPack,
+                 parent: Optional[Token] = None,
+                 child: Optional[Token] = None):
+        super().__init__(pack, parent, child)
+        self.dep_label: Optional[str] = None
+
+
+class RelationLink(Link):
+    """
+    A :class:`~ft.onto.base.Link` type entry which takes
+    :class:`~ft.onto.base_ontology.EntityMention` objects as parent and child.
+
+    Args:
+        pack (DataPack): the containing pack of this link.
+        parent (Entry, optional): the parent entry of the link.
+        child (Entry, optional): the child entry of the link.
+    """
+    ParentType = EntityMention
+    """The entry type of the parent node of :class:`RelationLink`."""
+    ChildType = EntityMention
+    """The entry type of the child node of :class:`RelationLink`."""
+
+    def __init__(
+            self,
+            pack: DataPack,
+            parent: Optional[EntityMention] = None,
+            child: Optional[EntityMention] = None):
+        super().__init__(pack, parent, child)
+        self.rel_type: Optional[Type] = None
+
+
+class Utterance(Annotation):
+    r"""An annotation based entry useful for dialogue.
+
+    Args:
+        pack (DataPack): The data pack this token belongs to.
+        begin (int): The offset of the first character in the entity mention.
+        end (int): The offset of the last character in the entity mention + 1.
+
+    """
+
+    def __init__(self, pack: DataPack, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.seq_num: str
---
+++ ft/onto/base_ontology_dev.py
@@ -0,0 +1,285 @@
+# ***automatically_generated***
+# flake8: noqa
+# mypy: ignore-errors
+# pylint: skip-file
+"""
+
+
+Automatically generated file. Do not change manually.
+"""
+import forte.data.data_pack
+import forte.data.ontology.top
+import ft.onto
+import typing
+
+
+__all__ = []
+
+
+__all__.extend('Token')
+
+
+class Token(forte.data.ontology.top.Annotation):
+    """
+    A span based annotation :class:`Token`, used to represent a token or a word.
+
+    Attributes:
+        pos (typing.Optional[str])
+        ud_xpos (typing.Optional[str])	Language specific pos tag. Used in CoNLL-U Format. Refer to https://universaldependencies.org/format.html
+        lemma (typing.Optional[str])	Lemma or stem of word form.
+        ner (typing.Optional[str])
+        sense (typing.Optional[str])
+        is_root (typing.Optional[bool])
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.pos: typing.Optional[str] = None
+        self.ud_xpos: typing.Optional[str] = None
+        self.lemma: typing.Optional[str] = None
+        self.ner: typing.Optional[str] = None
+        self.sense: typing.Optional[str] = None
+        self.is_root: typing.Optional[bool] = None
+
+    @property
+    def pos(self):
+        return self.pos
+
+    @pos.setter
+    def pos(self, pos: typing.Optional[str]):
+        self.set_fields(pos=pos)
+
+    @property
+    def ud_xpos(self):
+        return self.ud_xpos
+
+    @ud_xpos.setter
+    def ud_xpos(self, ud_xpos: typing.Optional[str]):
+        self.set_fields(ud_xpos=ud_xpos)
+
+    @property
+    def lemma(self):
+        return self.lemma
+
+    @lemma.setter
+    def lemma(self, lemma: typing.Optional[str]):
+        self.set_fields(lemma=lemma)
+
+    @property
+    def ner(self):
+        return self.ner
+
+    @ner.setter
+    def ner(self, ner: typing.Optional[str]):
+        self.set_fields(ner=ner)
+
+    @property
+    def sense(self):
+        return self.sense
+
+    @sense.setter
+    def sense(self, sense: typing.Optional[str]):
+        self.set_fields(sense=sense)
+
+    @property
+    def is_root(self):
+        return self.is_root
+
+    @is_root.setter
+    def is_root(self, is_root: typing.Optional[bool]):
+        self.set_fields(is_root=is_root)
+
+
+__all__.extend('Document')
+
+
+class Document(forte.data.ontology.top.Annotation):
+    """
+    A span based annotation `Document`, normally used to represent a document.
+
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, begin: int, end: int):
+        super().__init__(pack, begin, end)
+
+
+__all__.extend('PredicateArgument')
+
+
+class PredicateArgument(forte.data.ontology.top.Annotation):
+    """
+    A span based annotation `PredicateArgument`, normally used to represent an argument of a predicate, can be linked to the predicate via the predicate link.
+
+    Attributes:
+        ner_type (typing.Optional[str])
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.ner_type: typing.Optional[str] = None
+
+    @property
+    def ner_type(self):
+        return self.ner_type
+
+    @ner_type.setter
+    def ner_type(self, ner_type: typing.Optional[str]):
+        self.set_fields(ner_type=ner_type)
+
+
+__all__.extend('EntityMention')
+
+
+class EntityMention(forte.data.ontology.top.Annotation):
+    """
+    A span based annotation `EntityMention`, normally used to represent an Entity Mention in a piece of text.
+
+    Attributes:
+        ner_type (typing.Optional[str])
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.ner_type: typing.Optional[str] = None
+
+    @property
+    def ner_type(self):
+        return self.ner_type
+
+    @ner_type.setter
+    def ner_type(self, ner_type: typing.Optional[str]):
+        self.set_fields(ner_type=ner_type)
+
+
+__all__.extend('PredicateMention')
+
+
+class PredicateMention(forte.data.ontology.top.Annotation):
+    """
+    A span based annotation `PredicateMention`, normally used to represent a predicate (normally verbs) in a piece of text.
+
+    Attributes:
+        ner_type (typing.Optional[str])
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, begin: int, end: int):
+        super().__init__(pack, begin, end)
+        self.ner_type: typing.Optional[str] = None
+
+    @property
+    def ner_type(self):
+        return self.ner_type
+
+    @ner_type.setter
+    def ner_type(self, ner_type: typing.Optional[str]):
+        self.set_fields(ner_type=ner_type)
+
+
+__all__.extend('RelationLink')
+
+
+class RelationLink(forte.data.ontology.top.Link):
+    """
+    A `Link` type entry which represent a relation.
+
+    Attributes:
+        rel_type (typing.Optional[str])	The type of the relation.
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, parent: typing.Optional[forte.data.ontology.core.Entry] = None, child: typing.Optional[forte.data.ontology.core.Entry] = None):
+        super().__init__(pack, parent, child)
+        self.rel_type: typing.Optional[str] = None
+
+    @property
+    def rel_type(self):
+        return self.rel_type
+
+    @rel_type.setter
+    def rel_type(self, rel_type: typing.Optional[str]):
+        self.set_fields(rel_type=rel_type)
+
+
+__all__.extend('PredicateLink')
+
+
+class PredicateLink(forte.data.ontology.top.Link):
+    """
+    A `Link` type entry which represent a semantic role link between a predicate and its argument.
+
+    Attributes:
+        arg_type (typing.Optional[str])	The predicate link type.
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, parent: typing.Optional[forte.data.ontology.core.Entry] = None, child: typing.Optional[forte.data.ontology.core.Entry] = None):
+        super().__init__(pack, parent, child)
+        self.arg_type: typing.Optional[str] = None
+
+    @property
+    def arg_type(self):
+        return self.arg_type
+
+    @arg_type.setter
+    def arg_type(self, arg_type: typing.Optional[str]):
+        self.set_fields(arg_type=arg_type)
+
+
+__all__.extend('Dependency')
+
+
+class Dependency(forte.data.ontology.top.Link):
+    """
+    A `Link` type entry which represent a syntactic dependency.
+
+    Attributes:
+        dep_label (typing.Optional[str])	The dependency label.
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, parent: typing.Optional[forte.data.ontology.core.Entry] = None, child: typing.Optional[forte.data.ontology.core.Entry] = None):
+        super().__init__(pack, parent, child)
+        self.dep_label: typing.Optional[str] = None
+
+    @property
+    def dep_label(self):
+        return self.dep_label
+
+    @dep_label.setter
+    def dep_label(self, dep_label: typing.Optional[str]):
+        self.set_fields(dep_label=dep_label)
+
+
+__all__.extend('EnhancedDependency')
+
+
+class EnhancedDependency(forte.data.ontology.top.Link):
+    """
+    A `Link` type entry which represent a enhanced dependency: 
+     https://universaldependencies.org/u/overview/enhanced-syntax.html
+
+    Attributes:
+        dep_label (typing.Optional[str])	The enhanced dependency label in Universal Dependency.
+    """
+
+    def __init__(self, pack: forte.data.base_pack.PackType, parent: typing.Optional[forte.data.ontology.core.Entry] = None, child: typing.Optional[forte.data.ontology.core.Entry] = None):
+        super().__init__(pack, parent, child)
+        self.dep_label: typing.Optional[str] = None
+
+    @property
+    def dep_label(self):
+        return self.dep_label
+
+    @dep_label.setter
+    def dep_label(self, dep_label: typing.Optional[str]):
+        self.set_fields(dep_label=dep_label)
+
+
+__all__.extend('CoreferenceGroup')
+
+
+class CoreferenceGroup(forte.data.ontology.top.Group):
+    """
+    A group type entry that take `EntityMention`, as members, used to represent coreferent group of entities.
+
+    """
+
+    def __init__(self, pack: forte.data.container.EntryContainer, members: typing.Optional[typing.Set[forte.data.ontology.core.Entry]] = None):
+        super().__init__(pack, members)
