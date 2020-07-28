e348b44b3a08dff23c0756aa738c8e687d6c8dab
Hector <hunterhector@gmail.com>
2020-07-27 23:49:56 -0400

    Use the deploy keys to allow mirror push to Petuum (#214)
    * Add travis after success script.
    
    * Make sure shell script is executable
    
    * Add encrypted github deploy key.
    
    * Add encrypted Github deploy private key
    
    * update script to push mirroring

--- scripts/travis_after_success.sh
+++ scripts/travis_after_success.sh
@@ -3,10 +3,17 @@ echo "This script is run automatically after_success."
 
 ## The actions below tries to mirror the repo if this is run on the master branch of the main repoitory.
 mirror_from="https://github.com/asyml/forte.git"
-mirror_to="https://github.com/petuum/forte.git"
+mirror_to="git@github.com:petuum/forte.git"
 
 if [[ ${TRAVIS_BRANCH} == "master" ]] && [[ ${TRAVIS_PULL_REQUEST} == "false" ]] && [[ ${TRAVIS_REPO_SLUG} == "asyml/forte" ]]; then
   echo "Mirroring from "${mirror_from} " to "${mirror_to}
+
+  # Decrypt the private key.
+  openssl aes-256-cbc -k "$travis_key_password" -md sha256 -iter 100000 -d -a -in travis_key.enc -out ./travis_key
+  chmod 400 ./travis_key
+  echo "Host github.com" > ~/.ssh/config
+  echo "  IdentityFile $(pwd)/travis_key" >> ~/.ssh/config
+
   git clone --bare ${mirror_from} forte_bare
   if cd forte_bare; then
     git push --mirror ${mirror_to}
