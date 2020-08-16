e0784ae1cba03ab06303de26a0ed68fca86b90f1
ha-lins <shuailin97@gmail.com>
2020-08-16 09:44:40 +0800

    Remove the dependency from .travis.yml to setup.py and remove the version.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setuptools.setup(
         'txtgen': ['regex', 'tensorflow'],
         'stanza': ['stanza==1.0.1'],
         'test': ['ddt', 'testfixtures'],
-        'example': ['termcolor==1.1.0'],
+        'example': ['termcolor==1.1.0', 'hypothesis==2.0.0'],
         'wikipedia': ['rdflib==4.2.2'],
         'ir': ['faiss-cpu>=1.6.1', 'elasticsearch==7.5.1'],
         'spacy': ['spacy==2.3.0'],
