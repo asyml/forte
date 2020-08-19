690d3dabe126672008faed57262e7d3cf8410ef4
ha-lins <shuailin97@gmail.com>
2020-08-19 21:29:16 +0800

    Fix the download path error.

--- examples/Cliner/download_model.sh
+++ examples/Cliner/download_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # download the pre-trained model
-wget https://drive.google.com/file/d/1Jlm2wdmNA-GotTWF60zZRUs1MbnzYox2 -O CliNER/models/train_full.model
+wget https://drive.google.com/file/d/1Jlm2wdmNA-GotTWF60zZRUs1MbnzYox2 -O examples/Cliner/CliNER/models/train_full.model
 
 # download the dependency package for evaluation
-wget https://drive.google.com/file/d/1ZVgJ7EQtMjPpg_v-lCycCLdFgVzmdTxI -O CliNER/tools/i2b2va-eval.jar
+wget https://drive.google.com/file/d/1ZVgJ7EQtMjPpg_v-lCycCLdFgVzmdTxI -O examples/Cliner/CliNER/tools/i2b2va-eval.jar
