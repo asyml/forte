6a008141e5fdbb2ab54dbb124102807f04d0703f
yuhaoLucas <425151683@qq.com>
2020-09-16 20:18:34 -0400

    Separate the cast function to two steps.

--- forte/processors/base/data_augment_processor.py
+++ forte/processors/base/data_augment_processor.py
@@ -27,12 +27,21 @@ __all__ = [
 
 
 class BaseDataAugmentProcessor(Caster[DataPack, MultiPack]):
-    r"""The base class of processors that augments the data.
-    Pass the input data_pack to the "cast" method
-    and it will return an output Multipack.
+    r"""The base class of processors that augment data.
+    This processor will call :func:'augment_algo' to get the
+    augmented texts which are semantically similar to the original texts.
 
-    To inherit from this class, please override the augment function.
+    With the augmented texts, :func:'build_pack' builds the data packs by either
+        (1) Copying the original data pack and modifying it, or
+        (2) Building a new data pack from the augmented texts.
+    The implementation of :func:'build_pack' varies for different types of datasets.
+    For example, option (2) is available for datasets like sentiment analysis
+    where we can rebuild the annotations easily. But for treebanks where structure
+    information is stored in the original data pack, we can only rely on the option(1).
     """
+    def __init__(self):
+        super().__init__()
+
     def cast(self, pack: DataPack) -> MultiPack:
         """
         Augment the data-pack into a multi-pack.
@@ -45,14 +54,60 @@ class BaseDataAugmentProcessor(Caster[DataPack, MultiPack]):
         """
         p = MultiPack()
         p.add_pack_(pack, "original")
-        augmented_data: Iterable[Tuple[str, DataPack]] = self.augment(pack)
-        for pack_name, data_pack in augmented_data:
-            p.add_pack_(data_pack, pack_name)
+        augmented_texts: Iterable[Tuple[str, str]] = self.augment_algo(pack)
+        for pack_name, text in augmented_texts:
+            new_pack: DataPack = self.build_pack(pack, text)
+            p.add_pack_(new_pack, pack_name)
         return p
 
     @abstractmethod
-    def augment(self, pack: DataPack) -> Iterable[Tuple[str, DataPack]]:
-        r"""The method that augments the input datapack.
-        It returns tuples of pack name and pack data.
+    def augment_algo(self, pack: DataPack) -> Iterable[Tuple[str, str]]:
+        r"""The method that augments the input data pack.
+        Different algorithms can be inserted here,
+        such as word replacement or back-translation.
+        The outputs are raw strings, instead of Forte data structures.
+
+        Args:
+            pack: The data pack to be augmented.
+
+        Returns: An iterator of tuples [pack_name, augmented_text].
         """
         raise NotImplementedError
+
+    @abstractmethod
+    def build_pack(self, original_pack: DataPack, augmented_text: str):
+        r"""The method that builds a data pack
+        from both original data pack and augmented text.
+
+        For data pack where only annotations like Token, Sentence and Document
+        are included, it is simple to build a data pack only from the augmented text.
+
+        However, for datasets like treebanks, we need the tree structure information
+        embedded in the original data pack. We cannot infer that from the augmented text.
+        Instead of building a new data pack, We can only copy the original one and modify
+        it based on the augmented text. This will impose some constraints on the augmentation
+        algorithm, for example, only replacement-based methods can be used for treebanks
+        to preserve its original structure.
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def default_configs(cls):
+        """
+        This defines a basic config structure for AllenNLP.
+        :return: A dictionary with the default config for this processor.
+        Following are the keys for this dictionary:
+            - pack_builder_type: defines how :func:'build_pack' build new packs from
+            original data pack and augmented texts, e.g., it will build the annotations
+            only from the augmented text when pack_builder_type="plainText".
+            - augment_ontologies: defines what are the ontologies to build in
+            :func:'build_pack'. This should align with the "pack_builder_type".
+            For example, when the pack_builder_type="plainText", the augment_ontologies
+            must not include Link or ConstituencyNode.
+        """
+        config = super().default_configs()
+        config.update({
+            'pack_builder_type': "plainText",
+            'augment_ontologies': ["Token", "Sentence", "Document"]
+        })
+        return config
