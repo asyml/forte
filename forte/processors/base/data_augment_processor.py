629669b7df1345f91f597458b38c686a5a70ae9d
yuhaoLucas <425151683@qq.com>
2020-11-05 19:41:35 -0500

    add the processor

--- forte/processors/base/data_augment_processor.py
+++ forte/processors/base/data_augment_processor.py
@@ -60,8 +60,11 @@ class ReplacementDataAugmentProcessor(BaseDataAugmentProcessor):
         getting the augmented text, it will register the input & output
         for later batch process of building the new data pack.
         """
-        replaced_text: str = replacement_op.replace(input)
-        self.replaced_spans.append((input, replaced_text))
+        replaced_text: str
+        is_replace: bool
+        is_replace, replaced_text = replacement_op.replace(input)
+        if is_replace:
+            self.replaced_spans.append((input, replaced_text))
 
     def auto_align_annotations(
         self,
