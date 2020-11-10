e3247c33a7ab3459e867b5c877004dc48ac8f329
JiaqiangRuan <jiaqiang.ruan96@gmail.com>
2020-11-09 20:00:50 -0500

    RJQ: update reader

--- forte/data/readers/conll03_reader_new.py
+++ forte/data/readers/conll03_reader_new.py
@@ -122,9 +122,9 @@ class CoNLL03Reader(PackReader):
                 Sentence(pack, sentence_begin, offset - 1)
 
                 # Handle the last ner tag if exists.
-                # if prev_x is not None:
-                #     entity_mention = EntityMention(pack, start_index, offset-1)
-                #     entity_mention.ner_type = prev_y
+                if prev_x is not None:
+                    entity_mention = EntityMention(pack, start_index, offset-1)
+                    entity_mention.ner_type = prev_y
 
                 # Reset information.
                 sentence_cnt += 1
