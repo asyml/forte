998c1489184577dd6e5ea6f08e53599d3b693689
Mingkai Deng <dengmingkai@gmail.com>
2020-11-16 12:39:19 -0500

    Add BioBERT NER Predictor (#310)
    Adding an example for Bio NER.
    
    * Edit Subword ontology directly
    
    * Add BioBERT NER Predictor classes, facilities and examples
    
    * Improve code format after linting and add README.md
    
    * Update transformers_processor.py
    
    Fix pylint warnings.
    
    * Update __init__.py
    
    new line at the end of file.
    
    * Update ner_predictor.py
    
    A few pylint problems.
    
    * Update ner_predict_example.py
    
    Fix some flake8 problems.
    
    * Update ner_predictor.py
    
    mypy don't know all types.
    
    * Update ner_predictor.py
    
    pylint, flake8, mypy are all unhappy
    
    * Update transformers_processor.py
    
    import order
    
    * Update ner_predictor.py
    
    pylint,flake8 whitespaces
    
    Co-authored-by: Hector <hunterhector@gmail.com>

---
+++ forte/processors/transformers_processor.py
@@ -0,0 +1,60 @@
+# Copyright 2019 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, Any
+
+from nltk.tokenize.util import align_tokens
+from transformers import AutoTokenizer
+
+from forte.common.configuration import Config
+from forte.common.resources import Resources
+from forte.data.data_pack import DataPack
+from forte.processors.base import PackProcessor
+from ft.onto.base_ontology import Subword
+
+
+class BERTTokenizer(PackProcessor):
+    r"""A wrapper of BERT tokenizer.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.tokenizer = None
+
+    # pylint: disable=unused-argument
+    def initialize(self, resources: Resources, configs: Config):
+        self.tokenizer = AutoTokenizer.from_pretrained(configs.model_path)
+
+    def _process(self, input_pack: DataPack):
+        inputs = self.tokenizer(input_pack.text, return_tensors="pt")
+        tokens = self.tokenizer.convert_ids_to_tokens(
+                     inputs['input_ids'][0].tolist()
+                 )[1:-1]
+        tokens_clean = [token.replace('##', '') if token.startswith('##')
+                        else token for token in tokens]
+
+        for i, (begin, end) in enumerate(align_tokens(tokens_clean,
+                                                      input_pack.text.lower())):
+            subword = Subword(input_pack, begin, end)
+            subword.is_subword = tokens[i].startswith('##')
+
+    @classmethod
+    def default_configs(cls) -> Dict[str, Any]:
+        r"""Returns a `dict` of configurations of the processor with default
+        values. Used to replace the missing values of input ``configs`` during
+        pipeline construction.
+        """
+        config = super().default_configs()
+        config.update({'model_path': None})
+        return config
