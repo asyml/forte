b848a7893d3a74fbf9dfc695075302c13881725d
Yanwen Lin <lyw1124278064@gmail.com>
2020-12-17 19:28:00 -0500

    apply cherry-pick yanwen & RJQ: [main-train-tagging] move prediction to folder tagging;

--- examples/tagging/main_predict_tagging.py
+++ examples/tagging/main_predict_tagging.py
@@ -12,30 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """This file predict the ner tag for conll03 dataset."""
-
+import sys
 import yaml
 import torch
 from forte.pipeline import Pipeline
 from forte.data.readers.conll03_reader_new import CoNLL03Reader
 from forte.predictor import Predictor
-from ft.onto.base_ontology import Sentence, EntityMention
-from examples.ner_new.ner_evaluator import CoNLLNEREvaluator
+from ft.onto.base_ontology import Sentence, EntityMention, Token
+from examples.tagging.evaluator import CoNLLNEREvaluator
 
 
 def predict_forward_fn(model, batch):
     '''Use model and batch data to predict ner tag.'''
-    word = batch["text_tag"]["tensor"]
-    char = batch["char_tag"]["tensor"]
-    word_masks = batch["text_tag"]["mask"][0]
+    word = batch["text_tag"]["data"]
+    char = batch["char_tag"]["data"]
+    word_masks = batch["text_tag"]["masks"][0]
     output = model.decode(input_word=word, input_char=char, mask=word_masks)
     output = output.numpy()
-    return {'ner_tag': output}
+    return {'output_tag': output}
 
+
+task = sys.argv[1]
+assert task in ["ner", "pos"], \
+    "Not supported nlp task type: {}".format(task)
 
 config_predict = yaml.safe_load(open("configs/config_predict.yml", "r"))
 saved_model = torch.load(config_predict['model_path'])
 train_state = torch.load(config_predict['train_state_path'])
-
 
 reader = CoNLL03Reader()
 predictor = Predictor(batch_size=config_predict['batch_size'],
@@ -56,10 +59,14 @@ for pack in pl.process_dataset(config_predict['test_path']):
     print("---- pack ----")
     for instance in pack.get(Sentence):
         sent = instance.text
-        ner_tags = []
-        for entry in pack.get(EntityMention, instance):
-            ner_tags.append((entry.text, entry.ner_type))
+        output_tags = []
+        if task == "ner":
+            for entry in pack.get(EntityMention, instance):
+                output_tags.append((entry.text, entry.ner_type))
+        else:
+            for entry in pack.get(Token, instance):
+                output_tags.append((entry.text, entry.pos))
         print('---- example -----')
         print("sentence: ", sent)
-        print("ner_tags: ", ner_tags)
+        print("output_tags: ", output_tags)
     print(evaluator.get_result())
