9b8d1aa02926d8615a5e58d328a29443fccdc5e5
ziqian98 <luoziqian98@gmail.com>
2020-12-19 20:33:13 -0500

    LZQ: Reform redaer

--- forte/data/readers/imdb_reader.py
+++ forte/data/readers/imdb_reader.py
@@ -21,9 +21,6 @@ https://www.kaggle.com/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
 import logging
 import os
 from typing import Iterator
-from forte.common.exception import ProcessorConfigError
-from forte.common.configuration import Config
-from forte.common.resources import Resources
 from forte.data.data_utils_io import dataset_path_iterator
 from forte.data.data_pack import DataPack
 from forte.data.readers.base_reader import PackReader
@@ -44,15 +41,7 @@ class IMDBReader(PackReader):
         "movie comment, negative"
     """
 
-    def initialize(self, resources: Resources, configs: Config):
-        super().initialize(resources, configs)
-
-        if configs.imdb_file_extension is None:
-            raise ProcessorConfigError(
-                "Configuration imdb_file_extension not provided.")
-
     def _collect(self, *args, **kwargs) -> Iterator[str]:
-        # pylint: disable = unused-argument
         r"""Iterator over text files in the data_source
 
         Args:
@@ -64,7 +53,6 @@ class IMDBReader(PackReader):
 
         imdb_directory: str = args[0]
 
-        #imdb_file_extension: str = self.configs.imdb_file_extension
         imdb_file_extension = "imdb"
 
         logging.info("Reading dataset from %s with extension %s",
@@ -90,8 +78,6 @@ class IMDBReader(PackReader):
                     # Add sentence.
                     senobj = Sentence(pack, offset+1,
                                       offset + len(sentence)+1)
-                    #senobj.sentiment[sentence] = sentiment
-                    #senobj.__setattr__("sentim" , sentiment)
                     senobj.speaker = sentiment
 
                     # Add token
@@ -118,10 +104,3 @@ class IMDBReader(PackReader):
         pack.pack_name = file_path
 
         yield pack
-
-    @classmethod
-    def default_configs(cls):
-        config: dict = super().default_configs()
-        # Add imdb dataset file extension. The default is '.imdb'
-        config.update({'imdb_file_extension': 'imdb'})
-        return config
