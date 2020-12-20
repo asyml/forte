99978d63891b682ff644960f69cdde48a4774ee9
JiaqiangRuan <jiaqiang.ruan96@gmail.com>
2020-12-20 12:47:44 -0500

    RJQ: use "with" on opened file

--- forte/data/readers/conll03_reader.py
+++ forte/data/readers/conll03_reader.py
@@ -70,131 +70,131 @@ class CoNLL03Reader(PackReader):
         return os.path.basename(collection)
 
     def _parse_pack(self, collection: str) -> Iterator[DataPack]:
-        doc = open(collection, "r", encoding="utf8")
-        pack_id: int = 0
+        with open(collection, "r", encoding="utf8") as doc:
+            pack_id: int = 0
 
-        pack: DataPack = DataPack()
-        text: str = ""
-        offset: int = 0
-        has_rows: bool = False
+            pack: DataPack = DataPack()
+            text: str = ""
+            offset: int = 0
+            has_rows: bool = False
 
-        sentence_begin: int = 0
-        sentence_cnt: int = 0
+            sentence_begin: int = 0
+            sentence_cnt: int = 0
 
-        # NER tag is either "O" or in the format "X-Y",
-        # where X is one of B, I,
-        # Y is a tag like ORG, PER etc
-        prev_y = None
-        prev_x = None
-        start_index = -1
+            # NER tag is either "O" or in the format "X-Y",
+            # where X is one of B, I,
+            # Y is a tag like ORG, PER etc
+            prev_y = None
+            prev_x = None
+            start_index = -1
 
-        for line in doc:
-            line = line.strip()
+            for line in doc:
+                line = line.strip()
 
-            if line.find("DOCSTART") != -1:
-                # Skip the first DOCSTART.
-                if offset == 0:
-                    continue
-                # Add remaining sentence.
-                if has_rows:
-                    # Add the last sentence if exists.
+                if line.find("DOCSTART") != -1:
+                    # Skip the first DOCSTART.
+                    if offset == 0:
+                        continue
+                    # Add remaining sentence.
+                    if has_rows:
+                        # Add the last sentence if exists.
+                        Sentence(pack, sentence_begin, offset - 1)
+                        sentence_cnt += 1
+
+                    pack.set_text(text,
+                        replace_func=self.text_replace_operation)
+                    Document(pack, 0, len(text))
+                    pack.pack_name = collection + "_%d" % pack_id
+                    pack_id += 1
+                    yield pack
+
+                    # Create a new datapack.
+                    pack = DataPack()
+                    text = ""
+                    offset = 0
+                    has_rows = False
+
+                    sentence_begin = 0
+                    sentence_cnt = 0
+
+                    prev_y = None
+                    prev_x = None
+                    start_index = -1
+
+                elif line != "" and not line.startswith("#"):
+                    conll_components = line.split()
+
+                    word = conll_components[0]
+                    pos = conll_components[1]
+                    chunk_id = conll_components[2]
+
+                    ner_tag = conll_components[3]
+
+                    # A new ner tag occurs.
+                    if ner_tag == "O" or ner_tag.split("-")[0] == "B":
+                        # Add previous ner tag to sentence if it exists.
+                        if prev_y is not None:
+                            entity_mention = EntityMention(pack,
+                                                    start_index, offset - 1)
+                            entity_mention.ner_type = prev_y
+
+                        # Start process current ner tag.
+                        if ner_tag == "O":
+                            # Current ner tag is O, reset information.
+                            prev_x = None
+                            prev_y = None
+                            start_index = -1
+                        else:
+                            # Current ner tag is B.
+                            prev_x = "B"
+                            prev_y = ner_tag.split("-")[1]
+                            start_index = offset
+                    # This ner tag is connected to previous one.
+                    else:
+                        x, y = ner_tag.split("-")
+                        assert x == "I", "Unseen tag %s in the file." % x
+                        assert y == prev_y, "Error in %s." % ner_tag
+                        assert prev_x in ("B", "I"), "Error in %s." % ner_tag
+                        prev_x = "I"
+
+                    word_begin = offset
+                    word_end = offset + len(word)
+
+                    # Add tokens.
+                    token = Token(pack, word_begin, word_end)
+                    token.pos = pos
+                    token.chunk = chunk_id
+
+                    text += word + " "
+                    offset = word_end + 1
+                    has_rows = True
+                else:
+                    if not has_rows:
+                        # Skip consecutive empty lines.
+                        continue
+                    # Add sentence
                     Sentence(pack, sentence_begin, offset - 1)
-                    sentence_cnt += 1
 
-                pack.set_text(text, replace_func=self.text_replace_operation)
-                Document(pack, 0, len(text))
-                pack.pack_name = collection + "_%d" % pack_id
-                pack_id += 1
-                yield pack
-
-                # Create a new datapack.
-                pack = DataPack()
-                text = ""
-                offset = 0
-                has_rows = False
-
-                sentence_begin = 0
-                sentence_cnt = 0
-
-                prev_y = None
-                prev_x = None
-                start_index = -1
-
-            elif line != "" and not line.startswith("#"):
-                conll_components = line.split()
-
-                word = conll_components[0]
-                pos = conll_components[1]
-                chunk_id = conll_components[2]
-
-                ner_tag = conll_components[3]
-
-                # A new ner tag occurs.
-                if ner_tag == "O" or ner_tag.split("-")[0] == "B":
-                    # Add previous ner tag to sentence if it exists.
-                    if prev_y is not None:
-                        entity_mention = EntityMention(pack,
-                                                start_index, offset - 1)
+                    # Handle the last ner tag if exists.
+                    if prev_x is not None:
+                        entity_mention = EntityMention(pack, start_index,
+                                                        offset - 1)
                         entity_mention.ner_type = prev_y
 
-                    # Start process current ner tag.
-                    if ner_tag == "O":
-                        # Current ner tag is O, reset information.
-                        prev_x = None
-                        prev_y = None
-                        start_index = -1
-                    else:
-                        # Current ner tag is B.
-                        prev_x = "B"
-                        prev_y = ner_tag.split("-")[1]
-                        start_index = offset
-                # This ner tag is connected to previous one.
-                else:
-                    x, y = ner_tag.split("-")
-                    assert x == "I", "Unseen tag %s in the file." % x
-                    assert y == prev_y, "Error in %s." % ner_tag
-                    assert prev_x in ("B", "I"), "Error in %s." % ner_tag
-                    prev_x = "I"
+                    # Reset information.
+                    sentence_cnt += 1
+                    has_rows = False
+                    prev_y = None
+                    prev_x = None
+                    sentence_begin = offset
 
-                word_begin = offset
-                word_end = offset + len(word)
-
-                # Add tokens.
-                token = Token(pack, word_begin, word_end)
-                token.pos = pos
-                token.chunk = chunk_id
-
-                text += word + " "
-                offset = word_end + 1
-                has_rows = True
-            else:
-                if not has_rows:
-                    # Skip consecutive empty lines.
-                    continue
-                # Add sentence
+            if has_rows:
+                # Add the last sentence if exists.
                 Sentence(pack, sentence_begin, offset - 1)
-
-                # Handle the last ner tag if exists.
-                if prev_x is not None:
-                    entity_mention = EntityMention(pack, start_index,
-                                                    offset - 1)
-                    entity_mention.ner_type = prev_y
-
-                # Reset information.
                 sentence_cnt += 1
-                has_rows = False
-                prev_y = None
-                prev_x = None
-                sentence_begin = offset
 
-        if has_rows:
-            # Add the last sentence if exists.
-            Sentence(pack, sentence_begin, offset - 1)
-            sentence_cnt += 1
+            pack.set_text(text, replace_func=self.text_replace_operation)
+            Document(pack, 0, len(text))
+            pack.pack_name = collection
 
-        pack.set_text(text, replace_func=self.text_replace_operation)
-        Document(pack, 0, len(text))
-        pack.pack_name = collection
-
-        yield pack
-        doc.close()
+            yield pack
