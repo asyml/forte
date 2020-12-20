b55687d39f36d612422186ed375815ea4b41d75f
JiaqiangRuan <jiaqiang.ruan96@gmail.com>
2020-12-20 14:37:06 -0500

    RJQ: update documentation

--- forte/data/vocabulary.py
+++ forte/data/vocabulary.py
@@ -16,7 +16,7 @@ from typing import List, Tuple, Dict, Union, Hashable, Iterable
 
 
 class Vocabulary:
-    """This class will store "Elements" that are added, assign "Ids" to them and
+    r"""This class will store "Elements" that are added, assign "Ids" to them and
     return "Representations" if queried. These three are the main concepts in this class.
 
     1. Element: Any hashable instance that the user want to store.
@@ -32,22 +32,49 @@ class Vocabulary:
     2. The other one is <UNK> element, which, if added into the vocabulary, will be the
         default element if the queried element is not found.
 
-    Here is a table on how our Vocabulary class behavior under different settings.
+    Here is a table on how our Vocabulary class behavior under different settings. Ele0 means
+    the first element that is added to the vocabulary. Elements added later will be ele1, ele2
+    and so on. They will follow the same behavior as ele0. For readability, they are not listed
+    in the table.
 
-    User_request = {
-        "vocab method":  "raw"(handle outside), "indexing",      "indexing",          "one-hot",                    "one-hot"
-        "need_pad":           assume False,        True,            False,              True,                         False
-    }
+    .. list-table:: Vocabulary Behavior under different settings.
 
-    Vocabulary behavior:
-        get_pad_value            None                0              None                [0,0,0]                        None
-        inner_mapping            None           0:pad  1:ele0      0:ele0         -1:<PAD>   0:ele0                   0:ele0
-        element2repr           raise Error      pad->0 ele0->1     ele0->0      <PAD>->[0,0,0] ele0->[1,0,0]        ele0->[1,0,0]
-        id2element             raise Error      0->pad 1->ele0     0->ele0    -1 -> <PAD> 0->ele0 (be careful)        0->ele0
-
-    Not implemented for simplicity:
-        element2id             raise Error      pad->0 ele0->1     ele0->0        <PAD> -> -1   ele0 -> 0             ele0->0
-        repr2element           raise Error,     0->pad 1->ele0     0->ele0     [0,0,0]-><PAD> [1,0,0]->ele0        [1,0,0]->ele0
+        * - vocab_method
+          - raw (handle outside)
+          - indexing
+          - indexing
+          - one-hot
+          - one-hot
+        * - need_pad
+          - assume False
+          - True
+          - False
+          - True
+          - False
+        * - get_pad_value
+          - None
+          - 0
+          - None
+          - [0,0,0]
+          - None
+        * - inner_mapping
+          - None
+          - 0:pad 1:ele0
+          - 0:ele0
+          - -1:<PAD> 0:ele0
+          - 0:ele0
+        * - element2repr
+          - raise Error
+          - pad->0 ele0->1
+          - ele0->0
+          - <PAD>->[0,0,0] ele0->[1,0,0]
+          - ele0->[1,0,0]
+        * - id2element
+          - raise Error
+          - 0->pad 1->ele0
+          - 0->ele0
+          - -1 -> <PAD> 0->ele0 (be careful)
+          - 0->ele0
     """
     PAD_ELEMENT = "<PAD>"
     UNK_ELEMENT = "<UNK>"
@@ -72,16 +99,19 @@ class Vocabulary:
             self.add_element(Vocabulary.UNK_ELEMENT)
 
     def add_element(self, element: Hashable):
+        """This function will add element to the vocabulary."""
         if element not in self.element2id_dict:
             self.element2id_dict[element] = self.next_id
             self.id2element_dict[self.next_id] = element
             self.next_id += 1
 
     def id2element(self, idx: int) -> Hashable:
+        """This function will map id to element."""
         return self.id2element_dict[idx]
 
     def element2repr(self, element: Hashable) \
                     -> Union[int, List[int]]:
+        """This function will map element to representation."""
         if self.use_unk:
             idx = self.element2id_dict.get(element,
                     self.element2id_dict[Vocabulary.UNK_ELEMENT])
@@ -100,18 +130,23 @@ class Vocabulary:
             return vec
 
     def __len__(self) -> int:
+        """This function return the size of vocabulary."""
         return len(self.element2id_dict)
 
     def has_element(self, element: Hashable) -> bool:
+        """This function checks whether an element is added to vocabulary."""
         return element in self.element2id_dict
 
     def items(self) -> Iterable[Tuple[Hashable, int]]:
+        """This function will loop over the (element, id) pair."""
         return self.element2id_dict.items()
 
     def get_dict(self) -> Dict[Hashable, int]:
+        """This function will get the inner mapping from element to id."""
         return self.element2id_dict
 
     def get_pad_value(self) -> Union[None, int, List[int]]:
+        """This function will get the PAD element for the vocabulary."""
         if self.need_pad:
             return self.element2repr(self.PAD_ELEMENT)
         return None
