e319fe9a98c1995b6af1b0a877496467d2225047
jrxk <xiajianf@gmail.com>
2020-12-30 03:50:33 -0500

    update test

--- tests/forte/data/readers/largemovie_reader_test.py
+++ tests/forte/data/readers/largemovie_reader_test.py
@@ -43,22 +43,33 @@ class LargeMovieReaderTest(unittest.TestCase):
         # pos0 doc's leading text, neg1 doc's ending text.
         self.doc_text: Dict[str, str] = \
         {"pos":
-             "bromwell high is a cartoon comedy it ran at the same time as "
-             "some other programs about school life such as teachers my 35 "
-             "years in the teaching profession lead me to believe that "
-             "bromwell highs satire is much closer to reality than is",
+            'Bromwell High is a cartoon comedy. It ran at the same time'
+            ' as some other programs about school life, such as "Teache'
+            'rs". My 35 years in the teaching profession lead me to bel'
+            'ieve that Bromwell High\'s satire is much closer to reality '
+            'than is "Teachers". The scramble to survive financially, the'
+            ' insightful students who can see right through their pathetic'
+            ' teachers\' pomp, the pettiness of the whole situation, all '
+            'remind me of the schools I knew and their students. When I saw'
+            ' the episode in which a student repeatedly tried to burn down '
+            'the school, I immediately recalled ......... at .......... '
+            'High. A classic line: INSPECTOR: I\'m here to sack one of your '
+            'teachers. STUDENT: Welcome to Bromwell High. I expect that '
+            'many adults of my age think that Bromwell High is far fetched.'
+            ' What a pity that it isn\'t!',
          "neg":
-             "this new imdb rule of requiring ten lines for every review "
-             "when a movie is this worthless it doesnt require ten lines of "
-             "text to let other readers know that it is a waste of time "
-             "and tape avoid this movie"}
-        # pos3 sentence #1, neg3 sentence #5.
-        self.sent_text: Dict[str, str] = \
-            {"pos":
-                 "all the worlds a stage and its people actors in it",
-             "neg":
-                 "i was put through tears repulsion shock anger sympathy "
-                 "and misery when reading about the women of union street"}
+             'Robert DeNiro plays the most unbelievably intelligent '
+             'illiterate of all time. This movie is so wasteful of talent,'
+             ' it is truly disgusting. The script is unbelievable. The '
+             'dialog is unbelievable. Jane Fonda\'s character is a caricature'
+             ' of herself, and not a funny one. The movie moves at a snail\'s'
+             ' pace, is photographed in an ill-advised manner, and is '
+             'insufferably preachy. It also plugs in every cliche in the '
+             'book. Swoozie Kurtz is excellent in a supporting role, but so'
+             ' what?  Equally annoying is this new IMDB rule of requiring '
+             'ten lines for every review. When a movie is this worthless, '
+             'it doesn\'t require ten lines of text to let other readers '
+             'know that it is a waste of time and tape. Avoid this movie.'}
         # pos0 doc's score, neg1 doc's score.
         self.score: Dict[str, float] = \
             {"pos": 0.9,
@@ -84,15 +95,11 @@ class LargeMovieReaderTest(unittest.TestCase):
                 docid1 = self.doc_ids[dir][1]
                 if pack.pack_name == docid1:
                     for doc in pack.get(Document):
+                        print(doc.text)
                         self.assertIn(self.doc_text[dir], doc.text)
                         # test sentiments.
                         self.assertEqual(
                             doc.sentiment[docid1], self.score[dir])
-                # Test sentences.
-                elif pack.pack_name == docid0:
-                    sents = pack.get(Sentence)
-                    self.assertTrue(self.sent_text[dir] in
-                                    [sent.text for sent in sents])
 
                 count_packs += 1
 
