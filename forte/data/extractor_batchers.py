2569bdc0825ee3e9781428795e12b133fa90963d
Jiaqiang Ruan <jiaqiang.ruan96@gmail.com>
2021-01-20 11:12:18 -0500

    RJQ: [predictor] update implementation

---
+++ forte/data/extractor_batchers.py
@@ -0,0 +1,271 @@
+# Copyright 2019 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# pylint: disable=attribute-defined-outside-init
+
+from abc import abstractmethod
+from typing import (
+    Dict, List, Iterable, Union, Optional, Tuple, Type, Generic, Iterator, Any)
+
+from forte.common.configuration import Config
+from forte.data.base_pack import PackType
+from forte.data.data_pack import DataPack
+from forte.data.multi_pack import MultiPack
+from forte.data.types import DataRequest
+from forte.data.data_utils_io import merge_batches, batch_instances
+from forte.data.ontology.top import Annotation
+from forte.data.ontology.core import Entry
+
+# __all__ = [
+#     "ProcessingBatcher",
+#     "FixedSizeDataPackBatcher",
+#     "FixedSizeMultiPackProcessingBatcher",
+# ]
+
+
+class ProcessingBatcher(Generic[PackType]):
+    r"""This defines the basis interface of the Batcher used in
+    :class:`~forte.processors.base.batch_processor.BatchProcessor`. This Batcher
+    only batches data sequentially. It receives new packs dynamically and cache
+    the current packs so that the processors can pack prediction results into
+    the data packs.
+
+    Args:
+        cross_pack (bool, optional): whether to allow batches go across
+        data packs when there is no enough data at the end.
+    """
+
+    def __init__(self, cross_pack: bool = True):
+        self.current_batch: Dict = {}
+        self.data_pack_pool: List[PackType] = []
+        self.data_instance_pool: List[Annotation] = []
+        self.current_batch_sources: List[int] = []
+
+        self.cross_pack: bool = cross_pack
+
+    def initialize(self, config):
+        r"""The implementation should initialize the batcher and setup the
+        internal states of this batcher.
+        This batcher will be called at the pipeline initialize stage.
+
+        Returns:
+
+        """
+        self.scope = config.scope
+        self.feature_scheme = config.feature_scheme
+        self.current_batch.clear()
+        self.data_pack_pool.clear()
+        self.data_instance_pool.clear()
+        self.current_batch_sources.clear()
+
+    @abstractmethod
+    def _should_yield(self) -> bool:
+        r"""User should implement this based on the state of the batcher to
+        indicate whether the batch criteria is met and the batcher should yield
+        the current batch. For example, whether the number of instances reaches
+        the batch size.
+
+        Returns:
+
+        """
+        raise NotImplementedError
+
+    def convert(self, batch):
+        converted = {}
+        for tag, features in batch.items():
+            converter = self.feature_scheme[tag]["converter"]
+            data, masks = converter.convert(features)
+            converted[tag]["data"] = data
+            converted[tag]["mask"] = masks
+            converted[tag]["features"] = features
+        return converted
+
+    def flush(self) -> Iterator[Dict]:
+        r"""Flush the remaining data.
+
+        Returns:
+
+        """
+        if self.current_batch:
+            yield self.convert(self.current_batch)
+            self.current_batch = {}
+            self.current_batch_sources = []
+
+    def get_batch(
+            self, input_pack: PackType, context_type: Type[Annotation],
+            requests: DataRequest) -> Iterator[Dict]:
+        r"""Returns an iterator of data batches."""
+        # cache the new pack and generate batches
+        self.data_pack_pool.append(input_pack)
+
+        for (data_batch, instances, instance_num) in self._get_data_batch(input_pack):
+            self.current_batch = merge_batches(
+                [self.current_batch, data_batch])
+            self.data_instance_pool.extend(instances)
+            self.current_batch_sources.append(instance_num)
+
+            # Yield a batch on two conditions.
+            # 1. If we do not want to have batches from different pack, we
+            # should yield since this pack is exhausted.
+            # 2. We should also yield when the batcher condition is met:
+            # i.e. ``_should_yield()`` is True.
+            if not self.cross_pack or self._should_yield():
+                yield self.convert(self.current_batch)
+                self.current_batch = {}
+                self.current_batch_sources = []
+
+    def _get_data_batch(
+            self, data_pack: PackType) -> Iterable[Tuple[Dict, Annotation, int]]:
+        r"""Get data batches based on the requests.
+
+        Args:
+            data_pack: The data pack to retrieve data from.
+            context_type: The context type of the data pack.
+            requests: The request detail.
+            offset: The offset for get_data.
+
+        Returns:
+
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def default_configs(cls) -> Dict[str, Any]:
+        return {
+            "scope": None,
+            "feature_scheme": {}
+        }
+
+
+class FixedSizeDataPackBatcher(ProcessingBatcher[DataPack]):
+    def initialize(self, config: Config):
+        super().initialize(config)
+        self.batch_size = config.batch_size
+        self.batch_is_full = False
+
+    def _should_yield(self) -> bool:
+        return self.batch_is_full
+
+    def _get_data_batch(self, data_pack: DataPack) -> Iterable[Tuple[Dict, int]]:
+        r"""Try to get batches from a dataset  with ``batch_size``, but will
+        yield an incomplete batch if the data_pack is exhausted.
+
+        Returns:
+            An iterator of tuples ``(batch, cnt)``, ``batch`` is a dict
+            containing the required annotations and context, and ``cnt`` is
+            the number of instances in the batch.
+        """
+        instances: List[Dict] = []
+        current_size = sum(self.current_batch_sources)
+
+        # for data in data_pack.get_data(context_type, requests, offset):
+        #     instances.append(data)
+        #     if len(instances) == self.batch_size - current_size:
+        #         batch = batch_instances(instances)
+        #         self.batch_is_full = True
+        #         yield (batch, len(instances))
+        #         instances = []
+        #         self.batch_is_full = False
+
+        for instance in data_pack.get(self.scope):
+            features = {}
+            for tag, scheme in self.feature_scheme.items():
+                features[tag] = scheme['extractor'].extract(data_pack, instance)
+            instances.append(features)
+            if len(instances) == self.batch_size - current_size:
+                batch = batch_instances(instances)
+                yield (batch, len(instances))
+                instances = []
+                self.batch_is_full = False
+                current_size = sum(self.current_batch_sources)
+
+        # Flush the remaining data.
+        if len(instances) > 0:
+            batch = batch_instances(instances)
+            yield (batch, len(instances))
+
+    @classmethod
+    def default_configs(cls) -> Dict:
+        config = super().default_configs()
+        config.update({
+            "batch_size": 10
+        })
+        return config
+
+
+# class FixedSizeMultiPackProcessingBatcher(ProcessingBatcher[MultiPack]):
+#     r"""A Batcher used in ``MultiPackBatchProcessors``.
+
+#     The Batcher calls the ProcessingBatcher inherently on each specified
+#     data pack in the MultiPack.
+
+#     It's flexible to query MultiPack so we delegate the task to the subclasses
+#     such as:
+#         - query all packs with the same ``context`` and ``input_info``.
+#         - query different packs with different ``context``s and ``input_info``s.
+
+#     Since the batcher will save the data_pack_pool on the fly, it's not trivial
+#     to do batching and slicing multiple data packs in the same time
+#     """
+
+#     def __init__(self, cross_pack: bool = True):
+#         super().__init__(cross_pack)
+#         self.batch_is_full = False
+
+#     def initialize(self, config: Config):
+#         super().initialize(config)
+#         self.input_pack_name = config.input_pack_name
+#         self.batch_size = config.batch_size
+#         self.batch_is_full = False
+
+#     def _should_yield(self) -> bool:
+#         return self.batch_is_full
+
+#     # TODO: Principled way of get data from multi pack?
+#     def _get_data_batch(
+#             self, multi_pack: MultiPack, context_type: Type[Annotation],
+#             requests: Optional[Dict[Type[Entry], Union[Dict, List]]] = None,
+#             offset: int = 0) -> Iterable[Tuple[Dict, int]]:
+#         r"""Try to get batches of size ``batch_size``. If the tail instances
+#         cannot make up a full batch, will generate a small batch with the tail
+#         instances.
+
+#         Returns:
+#             An iterator of tuples ``(batch, cnt)``, ``batch`` is a dict
+#             containing the required annotations and context, and ``cnt`` is
+#             the number of instances in the batch.
+#         """
+#         input_pack = multi_pack.get_pack(self.input_pack_name)
+
+#         instances: List[Dict] = []
+#         current_size = sum(self.current_batch_sources)
+#         for data in input_pack.get_data(context_type, requests, offset):
+#             instances.append(data)
+#             if len(instances) == self.batch_size - current_size:
+#                 batch = batch_instances(instances)
+#                 self.batch_is_full = True
+#                 yield (batch, len(instances))
+#                 instances = []
+#                 self.batch_is_full = False
+
+#         if len(instances):
+#             batch = batch_instances(instances)
+#             yield (batch, len(instances))
+
+#     @classmethod
+#     def default_configs(cls) -> Dict:
+#         return {
+#             'batch_size': 10,
+#             'input_pack_name': 'source'
+#         }
---
+++ forte/processors/base/extractor_batch_processor.py
@@ -0,0 +1,291 @@
+# Copyright 2019 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+The processors that process data in batch.
+"""
+import itertools
+from abc import abstractmethod, ABC
+from typing import Dict, Optional, Type, Any
+
+from forte.common import Resources, ProcessorConfigError
+from forte.common.configuration import Config
+from forte.data import slice_batch
+from forte.data.base_pack import PackType
+from forte.data.batchers import ProcessingBatcher, FixedSizeDataPackBatcher
+from forte.data.data_pack import DataPack
+from forte.data.multi_pack import MultiPack
+from forte.data.ontology.top import Annotation
+from forte.data.types import DataRequest
+from forte.process_manager import ProcessJobStatus
+from forte.processors.base.base_processor import BaseProcessor
+
+# __all__ = [
+#     "BaseBatchProcessor",
+#     "BatchProcessor",
+#     "MultiPackBatchProcessor",
+#     "FixedSizeBatchProcessor",
+#     "FixedSizeMultiPackBatchProcessor"
+# ]
+
+
+class BaseBatchProcessor(BaseProcessor[PackType], ABC):
+    r"""The base class of processors that process data in batch. This processor
+    enables easy data batching via analyze the context and data objects. The
+    context defines the scope of analysis of a particular task.
+
+    For example, in
+    dependency parsing, the context is normally a sentence, in entity
+    coreference, the context is normally a document. The processor will create
+    data batches relative to the context.
+
+    Key fields in this processor:
+        - context_type (Annotation): define the context (scope) to process.
+        - input_info: A data request. Based on this input_info. If
+          `use_coverage_index` is set to true, the processor will build the
+          index based on the input information to speed up the entry
+          searching time.
+        - batcher: The processing batcher used for this processor.The batcher
+          will also keep track of the relation between the pack and the batch
+          data.
+        - use_coverage_index: If true, the index will be built based on the
+          input_info.
+    """
+
+    def __init__(self):
+        super().__init__()
+        # self.context_type: Type[Annotation] = self._define_context()
+        # self.input_info: DataRequest = self._define_input_info()
+        self.batcher: ProcessingBatcher = self.define_batcher()
+        self.use_coverage_index = False
+
+    def initialize(self, resources: Resources, configs: Optional[Config]):
+        super().initialize(resources, configs)
+
+        assert configs is not None
+        try:
+            self.batcher.initialize(configs.batcher)
+        except AttributeError as e:
+            raise ProcessorConfigError(
+                "Error in handling batcher config, please provide the "
+                "check the config to see if you have the key 'batcher'."
+            ) from e
+
+    # @staticmethod
+    # @abstractmethod
+    # def _define_context() -> Type[Annotation]:
+    #     r"""User should define the context type for batch processors here. The
+    #     context must be of type :class:`Annotation`, the processor will create
+    #     data batches with in the span of each annotations. For example, if the
+    #     context type is ``Sentence``, and the task is POS tagging, then each
+    #     batch will contain the POS tags for all words in the sentence.
+
+    #     The "context" parameter here has the same meaning as the
+    #     :meth:`get_data()` function in class :class:`DataPack`.
+    #     """
+    #     raise NotImplementedError
+
+    # @staticmethod
+    # @abstractmethod
+    # def _define_input_info() -> DataRequest:
+    #     r"""User should define the :attr:`input_info` for the batch processors
+    #     here. The input info will be used to get batched data for this
+    #     processor.
+
+    #     The request here has the same meaning as the
+    #     :meth:`get_data()` function in class :class:`DataPack`.
+    #     """
+    #     raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def define_batcher() -> ProcessingBatcher:
+        r"""Define a specific batcher for this processor.
+        Single pack :class:`BatchProcessor` initialize the batcher to be a
+        :class:`~forte.data.batchers.ProcessingBatcher`.
+        And :class:`MultiPackBatchProcessor` initialize the batcher to be a
+        :class:`~forte.data.batchers.MultiPackProcessingBatcher`.
+        """
+        raise NotImplementedError
+
+    def _process(self, input_pack: PackType):
+        r"""In batch processors, all data are processed in batches. So this
+        function is implemented to convert the input datapacks into batches
+        according to the Batcher. Users do not need to implement this function
+        but should instead implement ``predict``, which computes results from
+        batches, and ``pack_all``, which convert the batch results back to
+        datapacks.
+
+        Args:
+            input_pack: The next input pack to be fed in.
+        """
+        if self.use_coverage_index:
+            self._prepare_coverage_index(input_pack)
+
+        # for batch in self.batcher.get_batch(
+        #         input_pack, self.context_type, self.input_info):
+        for batch in self.batcher.get_batch(input_pack):
+            pred = self.predict(batch)
+            self.pack_all(pred)
+            self.update_batcher_pool(-1)
+
+        if len(self.batcher.current_batch_sources) == 0:
+            self.update_batcher_pool()
+
+        # update the status of the jobs. The jobs which were removed from
+        # data_pack_pool will have status "PROCESSED" else they are "QUEUED"
+        q_index = self._process_manager.current_queue_index
+        u_index = self._process_manager.unprocessed_queue_indices[q_index]
+        data_pool_length = len(self.batcher.data_pack_pool)
+        current_queue = self._process_manager.current_queue
+
+        for i, job_i in enumerate(
+                itertools.islice(current_queue, 0, u_index + 1)):
+            if i <= u_index - data_pool_length:
+                job_i.set_status(ProcessJobStatus.PROCESSED)
+            else:
+                job_i.set_status(ProcessJobStatus.QUEUED)
+
+    def flush(self):
+        for batch in self.batcher.flush():
+            pred = self.predict(batch)
+            self.pack_all(pred)
+            self.update_batcher_pool(-1)
+
+        current_queue = self._process_manager.current_queue
+
+        for job in current_queue:
+            job.set_status(ProcessJobStatus.PROCESSED)
+
+    @abstractmethod
+    def predict(self, data_batch: Dict) -> Dict:
+        r"""The function that task processors should implement. Make
+        predictions for the input ``data_batch``.
+
+        Args:
+              data_batch (dict): A batch of instances in our ``dict`` format.
+
+        Returns:
+              The prediction results in dict datasets.
+        """
+        pass
+
+    def pack_all(self, output_dict: Dict):
+        r"""Pack the prediction results ``output_dict`` back to the
+        corresponding packs.
+        """
+        start = 0
+        for i in range(len(self.batcher.data_pack_pool)):
+            pack_i = self.batcher.data_pack_pool[i]
+            instances_i = self.batcher.data_instance_pool[:self.batcher.current_batch_sources[i]]
+            self.batcher.data_instance_pool = self.batcher.data_instance_pool[self.batcher.current_batch_sources[i]:]
+            output_dict_i = slice_batch(output_dict, start,
+                                        self.batcher.current_batch_sources[i])
+            self.pack(pack_i, instances_i, output_dict_i)
+            start += self.batcher.current_batch_sources[i]
+            pack_i.add_all_remaining_entries()
+
+
+    @classmethod
+    def default_configs(cls) -> Dict[str, Any]:
+        super_config = super().default_configs()
+
+        super_config['batcher'] = cls.define_batcher().default_configs()
+
+        return super_config
+
+    @abstractmethod
+    def pack(self, pack: PackType, instances: Annotation, inputs) -> None:
+        r"""The function that task processors should implement.
+
+        Add corresponding fields to ``pack``. Custom function of how
+        to add the value back.
+
+        Args:
+            pack (PackType): The pack to add entries or fields to.
+            inputs: The prediction results returned by :meth:`predict`. You
+                need to add entries or fields corresponding to this prediction
+                results to ``pack``.
+        """
+        raise NotImplementedError
+
+    def update_batcher_pool(self, end: Optional[int] = None):
+        r"""Update the batcher pool in :attr:`data_pack_pool` from the
+        beginning to ``end`` (``end`` is not included).
+
+        Args:
+            end (int): Will do finishing work for data packs in
+                :attr:`data_pack_pool` from the beginning to ``end``
+                (``end`` is not included). If `None`, will finish up all the
+                packs in :attr:`data_pack_pool`.
+        """
+        # TODO: the purpose of this function is confusing, especially the -1
+        #  argument value.
+        if end is None:
+            end = len(self.batcher.data_pack_pool)
+        self.batcher.data_pack_pool = self.batcher.data_pack_pool[end:]
+        self.batcher.current_batch_sources = \
+            self.batcher.current_batch_sources[end:]
+
+    @abstractmethod
+    def _prepare_coverage_index(self, input_pack: PackType):
+        """
+        Build the coverage index for ``input_pack``. After building, querying
+          data in this pack will become more efficient.
+
+        The index will be built based on the `input_info` field.
+
+        Args:
+            input_pack: The pack to be built.
+
+        Returns:
+
+        """
+        pass
+
+
+class BatchProcessor(BaseBatchProcessor[DataPack], ABC):
+    r"""The batch processors that process :class:`DataPack`.
+    """
+
+    def _prepare_coverage_index(self, input_pack: DataPack):
+        for entry_type in self.input_info.keys():
+            input_pack.build_coverage_for(self.context_type, entry_type)
+
+
+class FixedSizeBatchProcessor(BatchProcessor, ABC):
+    @staticmethod
+    def define_batcher() -> ProcessingBatcher:
+        return FixedSizeDataPackBatcher()
+
+
+# class MultiPackBatchProcessor(BaseBatchProcessor[MultiPack], ABC):
+#     r"""This just defines the generic type to :class:`MultiPack`.
+#     The implemented batch processors will process :class:`MultiPack`.
+#     """
+
+#     def __init__(self):
+#         super().__init__()
+#         self.input_pack_name = None
+
+#     # TODO multi pack batcher need to be further studied.
+#     def _prepare_coverage_index(self, input_pack: MultiPack):
+#         for entry_type in self.input_info.keys():
+#             input_pack.packs[self.input_pack_name].build_coverage_for(
+#                 self.context_type, entry_type)
+
+
+# class FixedSizeMultiPackBatchProcessor(MultiPackBatchProcessor, ABC):
+#     @staticmethod
+#     def define_batcher() -> ProcessingBatcher:
+#         return FixedSizeDataPackBatcher()
