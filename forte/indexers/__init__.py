2cbd5c7aae4f5c3859cb4bdda3b17209f4f54261
Hector <hunterhector@gmail.com>
2021-02-05 11:49:19 -0500

    Reduce unwanted dependency temporarily (#375)
    * Currently do not auto import the indexer based classes into init.
    
    This is to avoid the core library to depend on the indexer libraries unexpectedly (elasticsearch and faiss)
    
    * Also remove the import * from data selector.
    
    * stable pip
    
    * fix import to direct import

--- forte/indexers/__init__.py
+++ forte/indexers/__init__.py
@@ -11,5 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from forte.indexers.elastic_indexer import *
-from forte.indexers.embedding_based_indexer import *
