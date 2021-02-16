4cb2f15650f1e27d86ab021973864dd888f867e2
hunterhector <hunterhector@gmail.com>
2021-02-16 18:57:56 -0500

    Fix a stave reader problem and update the doc.

--- tests/forte/data/readers/stave_reader_test.py
+++ tests/forte/data/readers/stave_reader_test.py
@@ -15,6 +15,32 @@ from forte.pipeline import Pipeline
 from forte.data.data_utils import maybe_download
 
 
+def query(sql_db, q: str):
+    conn = sqlite3.connect(sql_db)
+    c = conn.cursor()
+    return c.execute(q)
+
+
+def build_ontology(project_name):
+    onto_path = "./stave_test_onto"
+    res = query(f'SELECT ontology FROM nlpviewer_backend_project '
+                f'WHERE nlpviewer_backend_project.name = '
+                f'"{project_name}"').fetchone()[0]
+    with tempfile.NamedTemporaryFile('w') as onto_file:
+        onto_file.write(res)
+        OntologyCodeGenerator().generate(
+            onto_file.name, onto_path, lenient_prefix=True
+        )
+    # Make sure the newly created path is in the python path.
+    sys.path.append(onto_path)
+
+    # Make sure we can import the newly generated modules.
+    try:
+        importlib.import_module('edu.cmu')
+    except Exception:
+        pass
+
+
 @ddt
 class StaveReaderTest(unittest.TestCase):
     def setUp(self):
@@ -45,39 +71,17 @@ class StaveReaderTest(unittest.TestCase):
             c.executescript(q)
             conn.commit()
 
-    def _query(self, q: str):
-        conn = sqlite3.connect(self.sql_db)
-        c = conn.cursor()
-        return c.execute(q)
-
     @data('project-1-example', 'project-2-example')
     def test_stave_reader_project(self, project_name: str):
-        def build_ontology():
-            onto_path = "./stave_test_onto"
-            res = self._query(f'SELECT ontology FROM nlpviewer_backend_project '
-                              f'WHERE nlpviewer_backend_project.name = '
-                              f'"{project_name}"').fetchone()[0]
-            with tempfile.NamedTemporaryFile('w') as onto_file:
-                onto_file.write(res)
-                OntologyCodeGenerator().generate(
-                    onto_file.name, onto_path, lenient_prefix=True
-                )
-            # Make sure the newly created path is in the python path.
-            sys.path.append(onto_path)
-
-            # Make sure we can import the newly generated modules.
-            try:
-                importlib.import_module('edu.cmu')
-            except Exception:
-                pass
-
-        build_ontology()
+        build_ontology(project_name)
 
         # Query packs in this project directly.
-        pack_count: int = self._query(
+        pack_count: int = query(
+            self.sql_db,
             f"SELECT Count(*) FROM {self.datapack_table}, {self.project_table} "
             f"WHERE {self.datapack_table}.project_id = {self.project_table}.id "
-            f"AND {self.project_table}.name = '{project_name}'").fetchone()[0]
+            f"AND {self.project_table}.name = '{project_name}'"
+        ).fetchone()[0]
 
         # Read the data packs using the reader.
         nlp: Pipeline[DataPack] = Pipeline[DataPack]()
