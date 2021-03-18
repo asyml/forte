398766fb629ec60d6b6084984f028b56443bb6d8
Yishan Jiao <yishan.jiao@petuum.com>
2021-03-18 10:24:50 -0700

    use util get_class

--- forte/data/base_extractor.py
+++ forte/data/base_extractor.py
@@ -150,7 +150,7 @@ class BaseExtractor(ABC):
 
     @property
     def entry_type(self) -> object:
-        return locate(self.config.entry_type)
+        return get_class(self.config.entry_type)
 
     @property
     def vocab_method(self) -> str:
