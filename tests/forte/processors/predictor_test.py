fc4cab0e23a75b4017489347a2a036fd097c6ea8
Yishan Jiao <yishan.jiao@petuum.com>
2021-04-02 16:22:26 -0700

    remove request func

--- tests/forte/processors/predictor_test.py
+++ tests/forte/processors/predictor_test.py
@@ -11,7 +11,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import pdb
 import os
 import unittest
 from dataclasses import dataclass
