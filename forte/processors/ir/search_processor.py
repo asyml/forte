4295eddb546fc606761a812b57ce07ea660409c3
hunterhector <hunterhector@gmail.com>
2021-06-02 17:24:38 -0400

    Remove elastic dependency

--- forte/processors/ir/search_processor.py
+++ forte/processors/ir/search_processor.py
@@ -65,7 +65,8 @@ class SearchProcessor(MultiPackProcessor):
         config.update({
             'model_dir': None,
             'response_pack_name_prefix': 'doc',
-            'indexer_class': 'EmbeddingBasedIndexer',
+            'indexer_class': 'forte_wrapper.faiss.embedding_based_indexer'
+                             '.EmbeddingBasedIndexer',
             'indexer_configs': {
                 "index_type": "GpuIndexFlatIP",
                 "dim": 768,
