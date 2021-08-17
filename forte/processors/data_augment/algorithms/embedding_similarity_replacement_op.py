0bb3b498d1c5241384a51710a142c150acaf51b0
Jianfeng Xia <xiajianf@gmail.com>
2021-08-17 12:16:35 -0400

    Embedding similarity replacement op (#261)
    * Add embedding similarity augmenter
    
    * Update embedding similarity augmentor, add text analysis augment processor
    
    * Embedding similarity augmentor takes Embedding and Vocab as input, added util functions
    
    * update embedding similarity replacement op
    
    * update absolute file path for test
    
    * remove utils, add pipelien test
    
    * update parameters
    
    Co-authored-by: Hector <hunterhector@gmail.com>
    Co-authored-by: Jianfeng Xia <jianfengxia@Jianfengs-MacBook-Air.local>

---
+++ forte/processors/data_augment/algorithms/embedding_similarity_replacement_op.py
@@ -0,0 +1,81 @@
+# Copyright 2020 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+
+from typing import Tuple
+import numpy as np
+from texar.torch.data import Vocab, Embedding
+
+from ft.onto.base_ontology import Annotation
+from forte.common.configuration import Config
+from forte.processors.data_augment.algorithms.text_replacement_op \
+    import TextReplacementOp
+
+__all__ = [
+    "EmbeddingSimilarityReplacementOp",
+]
+
+
+class EmbeddingSimilarityReplacementOp(TextReplacementOp):
+    r"""
+    This class is a replacement op leveraging pre-trained word
+    embeddings, such as word2vec and glove, to replace the input
+    word with another word with similar word embedding.
+    By default, the replacement word is randomly chosen from the
+    top k words with the most similar embeddings.
+
+    Args:
+        configs:
+            The config should contain the following key-value pairs:
+            vocab_path (str): The absolute path to the vocabulary file for the
+                pretrained embeddings
+            embed_hparams (dict): The hparams to initialize the
+                texar.torch.data.Embedding object.
+            top_k (int): the number of k most similar words to choose from
+    """
+    def __init__(self, configs: Config):
+        super().__init__(configs)
+        self.vocab = Vocab(self.configs['vocab_path'])
+        embed_hparams = self.configs['embed_hparams']
+        embedding = Embedding(self.vocab.token_to_id_map_py, embed_hparams)
+        self.normalized_vectors = \
+            embedding.word_vecs / np.sqrt(
+                (embedding.word_vecs ** 2).sum(axis=1))[:, np.newaxis]
+
+    def replace(self, input: Annotation) -> Tuple[bool, str]:
+        r"""
+        This function replaces a word words with similar
+        pretrained embeddings.
+
+        Args:
+            input (Annotation): The input annotation.
+        Returns:
+            A tuple of two values, where the first element is a boolean value
+            indicating whether the replacement happens, and the second
+            element is the replaced word.
+        """
+        word = input.text
+        if word not in self.vocab.token_to_id_map_py:
+            return False, word
+
+        source_id = self.vocab.token_to_id_map_py[word]
+        source_vector = self.normalized_vectors[source_id]
+        scores = np.dot(self.normalized_vectors, source_vector)
+        target_ids = np.argpartition(
+            -scores, self.configs["top_k"] + 1)[:self.configs["top_k"] + 1]
+        target_words = [self.vocab.id_to_token_map_py[idx]
+            for idx in target_ids if idx != source_id and
+            self.vocab.id_to_token_map_py[idx].lower() != word.lower()]
+        return True, random.choice(target_words)
---
+++ tests/forte/processors/data_augment/algorithms/embedding_similarity_replacement_op_test.py
@@ -0,0 +1,130 @@
+# Copyright 2020 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for dictionary word replacement data augmenter.
+"""
+import os
+import unittest
+
+from ddt import ddt, data, unpack
+from texar.torch.data import Embedding, load_glove
+from texar.torch.data.vocabulary import Vocab
+
+from forte.data.caster import MultiPackBoxer
+from forte.data.multi_pack import MultiPack
+from forte.data.readers import StringReader
+from forte.data.selector import AllPackSelector
+from forte.pipeline import Pipeline
+from forte.processors.base import ReplacementDataAugmentProcessor
+from forte.processors.data_augment.algorithms.embedding_similarity_replacement_op \
+    import EmbeddingSimilarityReplacementOp
+from forte.processors.nltk_processors import NLTKWordTokenizer, NLTKPOSTagger
+
+from ft.onto.base_ontology import Token
+from forte.data.data_pack import DataPack
+
+
+@ddt
+class TestEmbeddingSimilarityReplacementOp(unittest.TestCase):
+    def setUp(self):
+        file_dir_path = os.path.dirname(__file__)
+        vocab_path = "tests/forte/processors/data_augment/algorithms/"\
+                     "sample_embedding.txt.vocab"
+        self.abs_vocab_path = os.path.abspath(os.path.join(file_dir_path,
+                                                      *([os.pardir] * 5),
+                                                      vocab_path))
+        embed_path = "tests/forte/processors/data_augment/algorithms/"\
+                     "sample_embedding.txt"
+        abs_embed_path = os.path.abspath(os.path.join(file_dir_path,
+                                                      *([os.pardir] * 5),
+                                                      embed_path))
+        embed_hparams = Embedding.default_hparams()
+        embed_hparams["file"] = abs_embed_path
+        embed_hparams["dim"] = 50
+        embed_hparams["read_fn"] = load_glove
+        self.embed_hparams = embed_hparams
+        self.esa = EmbeddingSimilarityReplacementOp(
+            configs={
+                'vocab_path': self.abs_vocab_path,
+                'embed_hparams': self.embed_hparams,
+                "top_k": 5,
+            }
+        )
+
+    def test_replace(self):
+        data_pack = DataPack()
+        data_pack.set_text("google")
+        token_1 = Token(data_pack, 0, 6)
+        data_pack.add_entry(token_1)
+        is_replace, replaced_token = self.esa.replace(token_1)
+        self.assertTrue(is_replace)
+        self.assertIn(
+            replaced_token,
+            ['yahoo', 'aol', 'microsoft', 'web', 'internet']
+        )
+
+    @data(
+        (["he google yahoo"],
+         ["his yahoo aol"],
+         )
+    )
+    @unpack
+    def test_pipeline(self, texts, expected_outputs):
+        nlp = Pipeline[MultiPack]()
+
+        boxer_config = {
+            'pack_name': 'input'
+        }
+
+        nlp.set_reader(reader=StringReader())
+        nlp.add(component=MultiPackBoxer(), config=boxer_config)
+        nlp.add(component=NLTKWordTokenizer(), selector=AllPackSelector())
+        nlp.add(component=NLTKPOSTagger(), selector=AllPackSelector())
+
+        processor_config = {
+            'augment_entry': "ft.onto.base_ontology.Token",
+            'other_entry_policy': {
+                'type': '',
+                'kwargs': {
+                    "ft.onto.base_ontology.Document": "auto_align",
+                    "ft.onto.base_ontology.Sentence": "auto_align"
+                }
+            },
+            'type': 'data_augmentation_op',
+            'data_aug_op': 'forte.processors.data_augment.algorithms.embedding_similarity_replacement_op.'
+                           'EmbeddingSimilarityReplacementOp',
+            'data_aug_op_config': {
+                'type': '',
+                'kwargs': {
+                    'vocab_path': self.abs_vocab_path,
+                    'embed_hparams': self.embed_hparams,
+                    "top_k": 1
+                },
+            },
+            'augment_pack_names': {
+                'kwargs': {
+                    'input': 'augmented_input'
+                }
+            }
+        }
+        nlp.add(component=ReplacementDataAugmentProcessor(), config=processor_config)
+        nlp.initialize()
+
+        for idx, m_pack in enumerate(nlp.process_dataset(texts)):
+            aug_pack = m_pack.get_pack('augmented_input')
+            self.assertEqual(aug_pack.text, expected_outputs[idx])
+
+
+if __name__ == "__main__":
+    unittest.main()
