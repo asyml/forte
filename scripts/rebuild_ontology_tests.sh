9597db37e21fb998f7e235bbbd66b93f1924e64b
feipenghe <hepengfe@uw.edu>
2021-09-09 10:31:47 -0700

    moved external ontology to ftx (#521)
    * moved external ontology to ftx
    
    * remove __init__.py
    
    * fix FileNotFoundError for test_top_attribute.py
    
    * delete __init__.py in ft/onto/ folder to solve the error that pytest cannot import pakcage
    
    * adding __init__.py properly to help pytest to import module
    
    * add a script to remove .generated files under tests folder
    
    * Update forte/ontology_specs/ag_news.json
    
    Co-authored-by: Hector <hunterhector@gmail.com>
    
    * fixed ontology json files
    
    * Update forte/ontology_specs/race_qa.json
    
    Co-authored-by: mylibrar <54747962+mylibrar@users.noreply.github.com>
    
    Co-authored-by: Hector <hunterhector@gmail.com>
    Co-authored-by: mylibrar <54747962+mylibrar@users.noreply.github.com>

--- scripts/rebuild_ontology_tests.sh
+++ scripts/rebuild_ontology_tests.sh
@@ -4,3 +4,6 @@ python -m forte.command_line.generate_ontology create -i tests/forte/data/ontolo
 python -m forte.command_line.generate_ontology create -i tests/forte/data/ontology/test_specs/example_complex_ontology.json -r -o tests/forte/data/ontology/test_outputs
 python -m forte.command_line.generate_ontology create -i tests/forte/data/ontology/test_specs/example_multi_module_ontology.json -r -o tests/forte/data/ontology/test_outputs
 python -m forte.command_line.generate_ontology create -i tests/forte/data/ontology/test_specs/race_qa_onto.json -r -o tests/forte/data/ontology/test_outputs
+python -m forte.command_line.generate_ontology create -i tests/forte/data/ontology/test_specs/test_top_attribute.json -r -o tests/forte/data/ontology/test_outputs
+# remove all .generated files in tests folder
+find ./tests -name ".generated" -delete
