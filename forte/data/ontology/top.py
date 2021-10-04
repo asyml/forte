2b0162b7d39908d24d0a93fe0c811e3f553238c0
hunterhector <hunterhector@gmail.com>
2021-10-04 19:14:34 -0400

    should not include pdb

--- forte/data/ontology/top.py
+++ forte/data/ontology/top.py
@@ -222,8 +222,6 @@ class Link(BaseLink):
         Args:
             parent: The parent entry.
         """
-        import pdb
-        pdb.set_trace()
         if not isinstance(parent, self.ParentType):
             raise TypeError(
                 f"The parent of {type(self)} should be an "
