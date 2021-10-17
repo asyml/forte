24846e8ec525d91d871a2f11f8c0b7878177463e
Arnav Parekhji <arnav17121999@gmail.com>
2021-10-17 16:13:30 -0700

    Final Documentation Changes

--- forte/processors/data_augment/algorithms/character_flip_op.py
+++ forte/processors/data_augment/algorithms/character_flip_op.py
@@ -29,14 +29,17 @@ __all__ = ["CharacterFlipOp"]
 class CharacterFlipOp(TextReplacementOp):
     r"""
     A uniform generator that randomly flips a character with a similar
-     looking character from a predefined dictionary. (For example: a -> @)
+    looking character from a predefined dictionary imported from
+    "https://github.com/facebookresearch/AugLy/blob/main/" +
+    "augly/text/augmenters/utils.py".
+    (For example: the cat drank milk -> t/-/3 c@t d12@nk m!|_1<).
 
     Args:
         string: input string whose characters need to be replaced,
         dict_path (str): the `url` or the path to the pre-defined
-                    typo json file.
+                    typo json file,
         configs: prob(float): The probability of replacement,
-                    should fall in [0, 1]
+                    should fall in [0, 1].
     """
 
     def __init__(self, configs: Union[Config, Dict[str, Any]]):
@@ -58,27 +61,30 @@ class CharacterFlipOp(TextReplacementOp):
 
     def _flip(self, char: str):
         r"""
-        Flips character with similar character from input dictionary
+        Flips character with similar character from input dictionary.
 
         Args:
-            char: input character
+            char: input character.
         Returns:
-            the modified character
+            the modified character.
         """
-        if char in self.data.keys():
+        if char in self.data:
             return random.choice(self.data[char])
         else:
             return char
 
     def replace(self, input_anno: Annotation) -> Tuple[bool, str]:
         r"""
-        Takes in the annotated string and randomly augments few characters
-        from it based on the probability value in the configs
+        Takes in the annotated string and performs the character
+        flip operation on it that randomly augments few characters
+        from it based on the probability value in the configs.
 
         Args:
-            input_anno: the input annotation
+            input_anno: the input annotation.
         Returns:
-            the final augmented string
+            A tuple with the first element being a boolean value indicating
+            whether the replacement happens, and the second element is the
+            final augmented string.
         """
         augmented_string = ""
         for char in input_anno.text:
