7cf547c72c0a28756f2750f4dedd78200e034a2d
Suqi Sun <suqi.sun@petuum.com>
2021-10-21 14:06:08 -0400

    Add more details inline comments

--- examples/blog_post_examples/pytorch_ecosystem.py
+++ examples/blog_post_examples/pytorch_ecosystem.py
@@ -12,13 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Example code for "Building an Information System for Clinical Notes"
+Example code for pytorch blog post
 """
 from fortex.spacy import SpacyProcessor
+
 from forte.data.readers import HTMLReader
 from forte.pipeline import Pipeline
 from forte.data.data_pack import DataPack
 from forte.processors.stave import StaveProcessor
+
 from ft.onto.base_ontology import Dependency
 from ftx.medical import MedicalEntityMention
 
@@ -32,12 +34,30 @@ def main():
         "</p></body>"
     )
 
-    # Assemble the pipeline
+
+    # Assemble the pipeline:
+    # A pipeline is consisted of a set of Components (readers and processors).
+    # The data flows in the pipeline as data packs, and each component will
+    # use or add information to the data packs.
     pipeline = Pipeline[DataPack]()
+
+    # Set HTMLReader as pipeline reader:
+    # HTMLReader takes in list of html strings, cleans the HTML tags and
+    # stores the cleaned text in pack.
     pipeline.set_reader(HTMLReader())
+
+    # Add SpacyProcessor to process the datapack:
+    # SpacyProcessor provides functions including sentence parsing, tokenize,
+    # POS tagging, lemmatization, NER, and medical entity linking. This
+    # processor will do user defined tasks according to configs.
     pipeline.add(SpacyProcessor(), config={
         "processors": ["sentence", "tokenize", "pos", "ner", "dep", "umls_link"]
     })
+
+    # Add StaveProcessor to visualize the processing results：
+    # StaveProcessor provides easy visualization for forte users. We can
+    # visualize datapack with annotations by inserting it into the forte
+    # pipeline.
     pipeline.add(StaveProcessor())
 
     # Initialize and run the pipeline
@@ -48,13 +68,15 @@ def main():
         for link in datapack.get(Dependency):
             parent = link.get_parent()
             child = link.get_child()
+            # Print out the dependency between parent and child
             print(
                 f"'{child.text}' has dependency '{link.dep_label}'"
                 f" of parent '{parent.text}'"
             )
 
-        # Get the UMLS links
+        # Retrieve all the MedicalEntityMention in datapack
         for medical_entity in datapack.get(MedicalEntityMention):
+            # Get and print out the UMLS links
             for umls_link in medical_entity.umls_entities:
                 print(umls_link)
 
