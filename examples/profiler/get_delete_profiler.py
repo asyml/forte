8fc5a9dc83eb036b84bb480015bbffd4565e4ba3
wanglec <49846115+wanglec@users.noreply.github.com>
2021-10-27 12:21:07 -0700

    add profiler (#550)
    Co-authored-by: wanglechuan-gif <wanglechuan@evocolabs.com>

--- examples/profiler/get_delete_profiler.py
+++ examples/profiler/get_delete_profiler.py
@@ -11,10 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import time
+
 import os
 import unittest
-from typing import List
+from typing import List, Tuple
+from time import time
 
 from forte.data.data_pack import DataPack
 from forte.data.readers.ontonotes_reader import OntonotesReader
