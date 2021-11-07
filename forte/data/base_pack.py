ca91b95feca84eeb995f454dadb76cad4df9b45a
VincentYaoMBZUAI <vincent.yao@mbzuai.ac.ae>
2021-11-07 11:55:36 +0400

    bug435_follow_up

--- forte/data/base_pack.py
+++ forte/data/base_pack.py
@@ -193,10 +193,17 @@ class BasePack(EntryContainer[EntryType, LinkType, GroupType]):
 
         if serialize_method == "jsonpickle":
             with _open(data_source, mode="rt") as f:  # type: ignore
-                pack = cls.from_string(f.read())
+                try:
+                    pack = cls.from_string(f.read())
+                except AttributeError as e:
+                    logging.exception('There was an Attribute Error; details are: %s' % e)
+
         else:
             with _open(data_source, mode="rb") as f:  # type: ignore
-                pack = pickle.load(f)
+                try:
+                    pack = pickle.load(f)
+                except AttributeError as e:
+                    logging.exception('There was an Attribute Error; details are %s' % e)
 
         return pack  # type: ignore
 
