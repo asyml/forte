0928e0634fa3c48197dee1f1d8342d08435070f2
wanglechuan-gif <wanglechuan@evocolabs.com>
2021-11-24 00:34:55 -0800

    update direct query methods

--- examples/profiler/direct_query_profiler.py
+++ examples/profiler/direct_query_profiler.py
@@ -6,16 +6,12 @@ import texar.torch as tx
 import codecs
 from transformers import BertTokenizer
 from nltk.tokenize import word_tokenize
+from nltk import pos_tag
 
-
-def preprocess(line):
-    sentence: List[str] = []
-    pos_tags: List[str] = []
-    conll_components = line.split()
 
 
 class OntonoteDataProcessing():
-    def __init__(self, file_path = "data_samples/profiler/combine_data/test_combine.gold_conll"):
+    def __init__(self, file_path = "data_samples/profiler/combine_data/development_combine.gold_conll"):
         root_path = os.path.abspath(
             os.path.join(
                 os.path.dirname(os.path.abspath(__file__)),
@@ -31,57 +27,80 @@ class OntonoteDataProcessing():
 
     def data_processing(self):
         sentence = []
-        pos_tags = []
+        document = []
         words = []
+        subword_token = []
+        NLTK_token = []
+        NLTK_tags = []
+
+        process_time = 0
         tokenize_time = 0
         get_sentence_time = 0
+        get_doc_time = 0
         get_token_entity_time = 0
         # get processed pack from dataset
         bg = time.time()
         
         with codecs.open(self.dataset_path, "r", encoding="utf8") as open_file:
-            bg = time.time()
             sen = ''
+            doc = ''
             for line in open_file:
+                bg = time.time()
                 line = line.strip()
+
+                if line.startswith("#end document"):
+                    document.append(doc)
+
                 if line != "" and not line.startswith("#"):
-                    # Non-empty line. Collect the annotation.
                     conll_components = line.strip().split()
-
                     sen += conll_components[3] + ' '
-                    pos_tag = conll_components[4]
-
                     words.append(conll_components[3])
-                    pos_tags.append(pos_tag)
-                if line == "":
-                    sentence.append(sen)
-                    sen = ''
-            process_time = time.time() - bg 
+                    process_time += time.time() - bg 
 
-        # either use NLTK word_tokenize or self.tokenizer( Bert Tokenizer )
+                    t3 = time.time()
+                    subword_token.append(self.tokenizer(conll_components[3], return_tensors="pt"))
+                    tokenize_time += time.time() - t3
+
+                if line == "":
+                    t4 = time.time()
+                    doc += sen
+                    sentence.append(sen)
+                    process_time += time.time() - t4
+
+                    t1 = time.time()
+                    text = word_tokenize(sen)
+                    NLTK_token.append(text)
+                    NLTK_tags.append(pos_tag(text))
+                    tokenize_time += time.time() - t1
+                    sen = ''
+
+        # query NLTK word_tokenize or self.tokenizer( subword Tokenizer )
+
         t0 = time.time()
         for sent in sentence:
-            # t1 = time.time()
-            # tok = word_tokenize(sent)
-            # tokenize_time += time.time() - t1
-            pass
-        get_sentence_time += time.time() - t0
+            pass    
+        get_sentence_time = time.time() - t0
+
+        t5 = time.time()
+        for d in document:
+            pass    
+        get_doc_time = time.time() - t5
  
         t2 = time.time()
-        for w in words:
-            t3 = time.time()
-            tok = self.tokenizer(w, return_tensors="pt")
-            tokenize_time += time.time() - t3
-            # pass
+        for s in NLTK_token:
+            pass
+        for t in NLTK_tags:
+            pass
+        for w in subword_token:  
+            pass
         get_token_entity_time = time.time() - t2
-        
-
 
         time_dict = {
             "process_time": process_time,
             "tokenize_time": tokenize_time,
             "get_sentence_time": get_sentence_time,
             "get_token_entity_time": get_token_entity_time,
+            "get_doc_time": get_doc_time
         }
         return time_dict
 
