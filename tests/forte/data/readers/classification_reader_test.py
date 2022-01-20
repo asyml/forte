6f0eecb5bdd0b8530103df7b787ec7a0c8156cd6
feipenghe <kern1996@outlook.com>
2022-01-20 00:54:20 -0800

    add test cases for classification dataset reader

---
+++ tests/forte/data/readers/classification_reader_test.py
@@ -0,0 +1,110 @@
+# Copyright 2019 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Tests for classification dataset reader.
+"""
+import os
+import unittest
+import csv
+from typing import Dict
+
+from forte.pipeline import Pipeline
+from ftx.onto.ag_news import Description
+from ft.onto.base_ontology import Document, Title
+from forte.common import Resources, ProcessorConfigError
+from forte.data.readers import ClassificationDatasetReader
+from forte.data.data_pack import DataPack
+from fortex.nltk import NLTKWordTokenizer, NLTKSentenceSegmenter
+from fortex.huggingface import ZeroShotClassifier
+
+class ClassificationDatasetReaderTest(unittest.TestCase):
+    def setUp(self):
+        self.pipeline = Pipeline()
+        self.sample_file1: str = os.path.abspath(
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                *([os.path.pardir] * 4),
+                "data_samples/amazon_review_polarity_csv/sample.csv"
+            )
+        )
+
+        self.expected_content: Dict[int, str] = {}
+        with open(self.sample_file1, encoding="utf-8") as f:
+            data = csv.reader(f, delimiter=",", quoting=csv.QUOTE_ALL)
+            for line_id, line in enumerate(data):
+                class_id, title, description = line
+                class_id = int(class_id)
+                self.expected_content[line_id] = (class_id, title, description)
+        self.class_names1 = ["negative", "positive"]
+        self.index2class1 = dict(enumerate(self.class_names1))
+
+        self.class_idx_to_name = {
+            1: "negative",
+            2: "positive",
+        }
+    def set_reader_wo_index2class(self):
+        self.pipeline.set_reader(ClassificationDatasetReader())
+    
+    def test_exceptions(self):
+        self.assertRaises(ProcessorConfigError,
+                          self.set_reader_wo_index2class())
+        
+
+    def test_classification_dataset_reader(self):
+        self.pipeline.set_reader(ClassificationDatasetReader(),
+                                 config={"index2class": self.index2class1,
+                                         "skip_first_line": False})
+        self.pipeline.add(NLTKSentenceSegmenter())
+        self.pipeline.add(NLTKWordTokenizer())
+        self.pipeline.add(ZeroShotClassifier(),
+                config= {"candidate_labels": self.class_names1})
+        self.pipeline.initialize()
+        for data_pack in self.pipeline.process_dataset(self.sample_file1):
+            (
+                expected_class_id,
+                expected_title,
+                expected_content,
+            ) = self.expected_content[data_pack.pack_name]
+            self.assertIsInstance(data_pack, DataPack)
+            # Test Article
+            doc_entries = list(data_pack.get(Document))
+            # in our example, we have one Document instance
+            # stores one input ontology
+            # and another Document instance stores all concatenated text
+            self.assertTrue(len(doc_entries) == 2) 
+            article: Document = doc_entries[0]
+            self.assertIsInstance(article, Document)
+            self.assertEqual(
+                article.text, expected_title + "\n" + expected_content
+            )
+            # Test CDocument Class
+            doc_class = article.document_class
+            self.assertTrue(len(doc_class) == 1)
+            # print(class_idx_to_name, expected_class_id)
+            self.assertEqual(
+                 doc_class[0], self.class_idx_to_name[expected_class_id]
+            )
+            # Test Title
+            title_entries = list(data_pack.get(Title))
+            self.assertTrue(len(title_entries) == 1)
+            title: Title = title_entries[0]
+
+            self.assertEqual(title.text, expected_title)
+            # Test Description
+            content: Document =  doc_entries[1]
+            self.assertEqual(content.text, expected_content)
+
+
+if __name__ == "__main__":
+    unittest.main()
