bad149e9becae129069b0d7ab084cbeb85e16fa9
Pushkar-Bhuse <pushkar.s.bhuse@gmail.com>
2022-02-02 01:12:08 -0800

    Black Formatting

--- tests/forte/processors/data_augment/algorithms/skeleton_op_test.py
+++ tests/forte/processors/data_augment/algorithms/skeleton_op_test.py
@@ -6,9 +6,8 @@ from forte.data.data_pack import DataPack
 from typing import Union, Dict, Any
 from forte.common.configuration import Config
 from forte.utils.utils import get_class
-from forte.processors.data_augment.algorithms.skeleton_op import (
-    SkeletonOp
-)
+from forte.processors.data_augment.algorithms.skeleton_op import SkeletonOp
+
 
 class DummyAugmenter(SkeletonOp):
     def __init__(self, configs: Union[Config, Dict[str, Any]]) -> None:
@@ -25,8 +24,12 @@ class DummyAugmenter(SkeletonOp):
             # Testing the insert annotations method
             new_text_1 = "There"
             new_text_2 = "Avoid"
-            insert_attempt_1 = self.insert_annotations(new_text_1, input_pack, 5)
-            insert_attempt_2 = self.insert_annotations(new_text_2, input_pack, 5)
+            insert_attempt_1 = self.insert_annotations(
+                new_text_1, input_pack, 5
+            )
+            insert_attempt_2 = self.insert_annotations(
+                new_text_2, input_pack, 5
+            )
 
             # Testing the delete annotations method
             delete_attempt_1 = self.delete_annotations(annotation_list[0])
@@ -34,11 +37,16 @@ class DummyAugmenter(SkeletonOp):
 
             # Testing the replace annotations method
             replaced_text = "Last"
-            replace_attempt_1 = self.replace_annotations(annotation_list[-1], replaced_text)
-            replace_attempt_2 = self.replace_annotations(annotation_list[-1], replaced_text)
+            replace_attempt_1 = self.replace_annotations(
+                annotation_list[-1], replaced_text
+            )
+            replace_attempt_2 = self.replace_annotations(
+                annotation_list[-1], replaced_text
+            )
             return True
         except:
             return False
+
 
 class TestSkeletonOp(unittest.TestCase):
     def setUp(self) -> None:
@@ -54,7 +62,6 @@ class TestSkeletonOp(unittest.TestCase):
         self.assertFalse(insert_list)
         self.assertFalse(delete_list)
         self.assertFalse(replace_list)
-
 
         # Testing the insert annotations method
         data_pack = DataPack()
@@ -75,26 +82,18 @@ class TestSkeletonOp(unittest.TestCase):
         delete_list = self.skeleton_op.deleted_annotation_status()
         replace_list = self.skeleton_op.replaced_annotation_status()
 
-        self.assertEqual(
-            list(insert_list[data_pack.pack_id].items()),
-            [(5, 5)]
-        )
+        self.assertEqual(list(insert_list[data_pack.pack_id].items()), [(5, 5)])
 
-        self.assertEqual(
-            list(delete_list[data_pack.pack_id]),
-            [token_1.tid]
-        )
+        self.assertEqual(list(delete_list[data_pack.pack_id]), [token_1.tid])
 
         expectations = [
-                (Span(token_1.begin, token_1.end), ""),
-                (Span(token_1.end, token_1.end), "There"),
-                (Span(token_3.begin, token_3.end), "Last"),
-            ]
+            (Span(token_1.begin, token_1.end), ""),
+            (Span(token_1.end, token_1.end), "There"),
+            (Span(token_3.begin, token_3.end), "Last"),
+        ]
 
-        self.assertEqual(
-            list(replace_list[data_pack.pack_id]),
-            expectations
-        )
+        self.assertEqual(list(replace_list[data_pack.pack_id]), expectations)
+
 
 if __name__ == "__main__":
     unittest.main()
