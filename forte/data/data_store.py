f0d5b84aa2264a70529504fd38bb15e532a032ef
wanglec <49846115+wanglec@users.noreply.github.com>
2022-02-02 13:11:13 -0800

    reformat with black

--- forte/data/data_store.py
+++ forte/data/data_store.py
@@ -138,7 +138,9 @@ class DataStore(BaseStore):
         """
         self.entry_dict: dict = {}
 
-    def _new_annotation(self, entry_type: Union[str, Type[EntryType]], begin: int, end: int):
+    def _new_annotation(
+        self, entry_type: Union[str, Type[EntryType]], begin: int, end: int
+    ):
         r"""This function generates a new annotation with default fields.
         Called by add_annotation_raw() to create a new annotation
         with `type`, `begin`, and `end`.
