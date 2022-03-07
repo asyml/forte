149093d1973d46791cf515e320bd70bfd9538e5c
Xuezhi-Liang <xuezhi.liang.cs@gmail.com>
2022-03-07 10:21:31 +0400

    570_4

--- tests/forte/data/entry_type_generator_test.py
+++ tests/forte/data/entry_type_generator_test.py
@@ -1,5 +1,4 @@
 import unittest
-from unittest.mock import patch
 from forte.data.entry_type_generator import EntryTypeGenerator
 from forte.data.ontology.top import Annotation, Generics
 from typing import Optional, Dict
@@ -84,15 +83,6 @@ class SingleMetricTest(MetricTest):
 class EntryTypeGeneratorTest(unittest.TestCase):
     def setUp(self) -> None:
         pass
-
-    @patch("forte.data.entry_type_generator._get_type_attributes")
-    def test_get_type_attributes(self, mock_get_type_attributes):
-        # test that _get_type_attributes() is called only once
-        EntryTypeGenerator.get_type_attributes()
-        EntryTypeGenerator.get_type_attributes()
-        self.assertLessEqual(mock_get_type_attributes.call_count, 1)
-        EntryTypeGenerator.get_type_attributes()
-        self.assertLessEqual(mock_get_type_attributes.call_count, 1)
 
     def test_get_entry_attribute_by_class(self):
         entry_name_attributes_dict = {
