dbeea08a474f255c75a321e72ae125baf07b83a5
Xuezhi-Liang <xuezhi.liang.cs@gmail.com>
2022-03-07 16:09:59 +0400

    570_4

--- forte/data/entry_type_generator.py
+++ forte/data/entry_type_generator.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import functools
 from forte.utils.utils import get_class
 
