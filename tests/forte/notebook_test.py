ad5f45646b21289ed3d9cc4f234e4420b3dd498a
Pengfei He <hepengfe@gmail.com>
2022-03-22 08:39:10 -0700

    remove notebook test

--- tests/forte/notebook_test.py
+++
@@ -1,9 +0,0 @@
-from testbook import testbook
-
-
-@testbook(
-    "docs/notebook_tutorial/wrap_MT_inference_pipeline.ipynb", execute=False
-)
-def test_wrap_MT_inference_pipeline(tb):
-    # if we just want to run through the notebook
-    pass
