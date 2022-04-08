c7c5473ec58a830cb7074296080155c3be7b00dd
Hector <hunterhector@gmail.com>
2022-04-08 13:35:44 -0400

    Update texar_nondependency_test.py

--- tests/forte/texar_nondependency_test.py
+++ tests/forte/texar_nondependency_test.py
@@ -1,42 +1,25 @@
 """
-Unit tests for dependency test.
+Test cases to ensure native Forte code can be imported 
+with only backbone Forte library installed (without any
+extra imports).
 """
-from importlib import import_module
 import os
 import unittest
 
 
-class DependencyTest(unittest.TestCase):
-    def setUp(self):
-        self.dataset_path = os.path.abspath(
-            os.path.join(
-                os.path.dirname(os.path.realpath(__file__)),
-                *([os.path.pardir] * 4),
-                "data_samples/squad_v2.0/dev-v2.0-sample.json"
-            )
-        )
+class ForteImportTest(unittest.TestCase):        
+    def test_basic_import(self):
+        from forte import Pipeline
+        from forte.data import DataPack
 
     def test_import_data(self):
-        # Read with no replacements
-        import_module("forte.data")
-        import_module("forte.data.converter")
-        import_module("forte.data.extractors")
-        import_module("forte.data.vocabulary")
-        import_module("forte.data.readers")
-
-    def test_import_datasets(self):
-        import_module("forte.datasets")
-
-    def test_import_models(self):
-        import_module("forte.models")
-        import_module("forte.models.da_rl")
-        import_module("forte.models.ner")
-        import_module("forte.models.srl")
-        import_module("forte.models.srl_new")
+        import forte.data.readers.TerminalReader
+        import forte.datasets.mrc.squad_reader.SquadReader
 
     def test_import_processors(self):
-        import_module("forte.processors.data_augment.algorithms")
-        import_module("forte.processors.ir")
-        import_module("forte.processors.misc")
-        import_module("forte.processors.nlp")
-        import_module("forte.processors.third_party")
+        from forte.processors.writers import PackIdJsonPackWriter
+        from forte.processors.third_party import MicrosoftBingTranslator
+        from forte.processors.nlp import ElizaProcessor
+        from forte.processors.misc import AnnotationRemover
+
+
