858cc6cc8761d66f88a08f2fa33b045cc22a979e
Pengfei He <hepengfe@gmail.com>
2022-04-18 12:30:58 -0700

    Remove unnecessary comments

--- tests/forte/texar_nondependency_test.py
+++ tests/forte/texar_nondependency_test.py
@@ -24,27 +24,27 @@ class ForteImportTest(unittest.TestCase):
     def test_import_processors(self):
         from forte.processors.writers import (
             PackIdJsonPackWriter,
-        )  # check dependency on torch
+        )
         from forte.processors.nlp import (
             ElizaProcessor,
-        )  # check dependency on torch
+        )
         from forte.processors.misc import (
             AnnotationRemover,
-        )  # check dependency on torch
+        )
         from forte.processors.base import (
             BaseProcessor,
-        )  # check dependency on torch
+        )
         from forte.processors.data_augment import (
             BaseDataAugmentProcessor,
-        )  # check dependency on torch
+        )
         from forte.processors.ir.search_processor import (
             SearchProcessor,
-        )  # check dependency on torch
+        )
 
     def test_import_evaluator(self):
         from forte.evaluation.ner_evaluator import (
             CoNLLNEREvaluator,
-        )  # check dependency on torch
+        )
         from forte.evaluation.base import Evaluator
 
     def test_import_models(self):
@@ -53,9 +53,7 @@ class ForteImportTest(unittest.TestCase):
             Span,
             RawExample,
             Example,
-        )  # check dependency on torch
-
-        # pass
+        )
 
     def test_import_trainer(self):
-        from forte.trainer.base import BaseTrainer  # check dependency on torch
+        from forte.trainer.base import BaseTrainer
