a7da20b304434c55020deca4faff4140f7dc0ae7
Pengfei He <hepengfe@gmail.com>
2022-04-18 11:51:44 -0700

    fix MatrixLike importing

--- forte/data/extra_types.py
+++ forte/data/extra_types.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Dict, List, Tuple, Type, Union
+from typing import List, Union
 
 import torch
 import numpy as np
