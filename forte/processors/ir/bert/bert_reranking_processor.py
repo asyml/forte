44928a888b4dda32f1a3e9d7075079961af0b4dd
Pengfei He <hepengfe@gmail.com>
2022-04-20 18:39:13 -0700

    try import torch in forte/processors/ir/bert/bert_reranking_processor.py

--- forte/processors/ir/bert/bert_reranking_processor.py
+++ forte/processors/ir/bert/bert_reranking_processor.py
@@ -16,7 +16,14 @@
 import os
 from typing import Dict, Any
 
-import torch
+try:
+    import torch
+except ImportError as e1:
+    raise ImportError(
+        " `pytorch` is not installed correctly."
+        " Please refer to [extra requirement for ir processors](pip install forte[ir])"
+        " for more information. "
+    ) from e1
 
 from forte.common.configuration import Config
 from forte.common.resources import Resources
