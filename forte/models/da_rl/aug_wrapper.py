bdc4bc9348e06a38eb471143964113da1372238e
Pengfei He <hepengfe@gmail.com>
2022-04-20 18:27:41 -0700

    try import torch in forte/models/da_rl/aug_wrapper.py

--- forte/models/da_rl/aug_wrapper.py
+++ forte/models/da_rl/aug_wrapper.py
@@ -20,10 +20,17 @@ import math
 from typing import Tuple, Dict, Generator
 
 
-import torch
-from torch import nn
-from torch.nn import functional as F
-from torch.optim import Optimizer
+try:
+    import torch
+    from torch import nn
+    from torch.nn import functional as F
+    from torch.optim import Optimizer
+except ImportError as e:
+    raise ImportError(
+        " `pytorch` is not installed correctly."
+        " Please refer to [extra requirement for aug wrapper](pip install forte[models])"
+        " for more information. "
+    ) from e
 
 from forte.models.da_rl.magic_model import MetaModule
 
