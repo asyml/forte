cc2e63f806863cfea6e3bff963b2419fa0c01a11
feipenghe <hepengfe@gmail.com>
2022-04-26 15:39:41 -0700

    Move out dependency of rest modules in extra_requires in forte (#760)
    * import error for wikipedia
    
    * import error for transformers
    
    * import error for tqdm
    
    * add more import tests
    
    * remove pytorch installation at .github/workflows/main.yml
    
    * test backbone in an independent job at .github/workflows/main.yml
    
    * install pytest in .github/workflows/main.yml
    
    * remove RemoteProcessor that requires extra module at tests/forte/forte_backbone_test.py
    
    * add requests in remoe extra require at setup.py
    
    * remove init import RemoteProcessor at forte/processors/misc/__init__.py
    
    * try-except ImportError for requests at forte/processors/misc/remote_processor.py
    
    * change relative importing path for RemoteProcessor at tests/forte/remote_processor_test.py
    
    * try-except ImportError for module requests at forte/processors/third_party/machine_translation_processor.py
    
    * try-except ImportError for module requests at forte/processors/data_augment/algorithms/character_flip_op.py
    
    * try-except ImportError for module requests at forte/processors/data_augment/algorithms/distribution_replacement_op.py
    
    * add requests requirement in forte[data_aug] in setup.py
    
    * try-except ImportError for module requests at forte/processors/data_augment/algorithms/typo_replacement_op.py
    
    * pylint: raise errors from errors and disable import-outside-toplevel
    
    * pylint: raise errors from errors and disable import-outside-toplevel
    
    * forte_backbone_test.py  pytest -> regular running python script
    
    * move requests importing inside init for RemoteProcessor
    
    * revert remove init import RemoteProcessor at forte/processors/misc/__init__.py
    
    * remove tensorflow and pytorch version in the test_backbone job
    
    * simplify test_backbone job

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ if sys.version_info < (3, 6):
     sys.exit("Python>=3.6 is required by Forte.")
 
 version = {}
-with open(os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "forte/version.py"
-)) as fp:
+with open(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "forte/version.py")
+) as fp:
     exec(fp.read(), version)
 
 setuptools.setup(
@@ -50,12 +50,10 @@ setuptools.setup(
             "transformers>=4.15.0",
             "nltk",
             "texar-pytorch>=0.1.4",
+            "requests",
         ],
         "ir": ["texar-pytorch>=0.1.4", "tensorflow>=1.15.0"],
-        "remote": [
-            "fastapi>=0.65.2",
-            "uvicorn>=0.14.0",
-        ],
+        "remote": ["fastapi>=0.65.2", "uvicorn>=0.14.0", "requests"],
         "audio_ext": ["soundfile>=0.10.3"],
         "stave": ["stave>=0.0.1.dev12"],
         "models": [
