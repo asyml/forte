8af7f5acd8e89efe7393b2e5a633e0c076e3e7b4
feipenghe <hepengfe@gmail.com>
2022-05-25 12:14:53 -0700

    Datastore entry method (#771)
    * implment all_entries and num_entries methods and their test cases
    
    * add docstring for methods
    
    * add count's initial value
    
    * correct entry method to get subclasses of requested entry type
    
    * add check for Group entry
    
    * exclude link and group types while geting entries
    
    * add num_entries check after deleting an entry
    
    * allow non-annotation-like entries in all_entris and num_entries and minus the deletion count for non-annotation-like entries
    
    * update implementations of all_entries and num_entries based on the new DataStore implementation
    
    * update tests of all_entries and num_entries based on the new DataStore implementation
    
    * black
    
    * add class method _get_all_subclass and edit other methods based on it
    
    * sort self.__elements.keys() in _get_all_subclass
    
    * simplify implementation of num_entries based on _get_all_subclass
    
    * add test cases based on code review
    
    * use iter() in all_entries() to skip None

--- forte/data/data_store.py
+++ forte/data/data_store.py
@@ -470,6 +470,29 @@ class DataStore(BaseStore):
                 else:
                     return False
 
+    def _get_all_subclass(self, entry_type_name: str, inclusive: bool = False):
+        """
+        Get all subclasses of ``entry_type_name``.
+
+        Args:
+            entry_type_name (str): subclasses of entry of ``entry_type_name``
+                will be yielded if it's in ``DataStore`` storage.
+            inclusive: if it's True, then ``entry_type_name`` itself will be
+                yielded. False otherwise.
+
+
+        Yields:
+            subclass entry type name of ``entry_type_name``
+        """
+        for entry_type_key in sorted(self.__elements.keys()):
+            if (
+                entry_type_key == entry_type_name and inclusive
+            ) or self._is_subclass(
+                entry_type_key,
+                get_class(entry_type_name),
+            ):
+                yield entry_type_key
+
     def _is_annotation(self, type_name: str) -> bool:
         r"""This function takes a type_name and returns whether a type
         is an annotation type or not.
@@ -483,6 +506,40 @@ class DataStore(BaseStore):
         # TODO: use is_subclass() in DataStore to replace this
         entry_class = get_class(type_name)
         return issubclass(entry_class, (Annotation, AudioAnnotation))
+
+    def all_entries(self, entry_type_name: str) -> Iterator[List]:
+        """
+        Retrieve all entry data of entry type ``entry_type_name`` and
+        entries of subclasses of entry type ``entry_type_name``.
+
+        Args:
+            entry_type_name (str): the type name of entries that the User wants to retrieve.
+
+        Yields:
+            Iterator of raw entry data in list format.
+        """
+        for entry_type_key in self._get_all_subclass(entry_type_name, True):
+            yield from self.iter(entry_type_key)
+
+    def num_entries(self, entry_type_name: str) -> int:
+        """
+        Compute the number of entries of given ``entry_type_name`` and
+        entries of subclasses of entry type ``entry_type_name``.
+
+        Args:
+            entry_type_name (str): the type name of entries that the User wants to get its count.
+
+        Returns:
+            The number of entries of given ``entry_type_name``.
+        """
+        count = 0
+        for entry_type_key in self._get_all_subclass(entry_type_name, True):
+            count += len(self.__elements[entry_type_key])
+            # if non-annotation-like entries_type_name
+            # we need to minus the corresponding delete count
+            if entry_type_key in self.__deletion_count:
+                count -= self.__deletion_count[entry_type_key]
+        return count
 
     def _add_entry_raw(
         self,
@@ -924,9 +981,7 @@ class DataStore(BaseStore):
             type_names: a list of string type names
 
         Returns:
-
             An iterator of entry elements.
-
         """
 
         n = len(type_names)
