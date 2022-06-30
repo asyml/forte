0007f78407ac2b519caec593dbb10889c5a2edf8
Pengfei He <hepengfe@gmail.com>
2022-06-29 22:17:20 -0700

    add test cases for box out of the image

--- tests/forte/image_annotation_test.py
+++ tests/forte/image_annotation_test.py
@@ -15,7 +15,9 @@
 Unit tests for ImageAnnotation.
 """
 import unittest
+from forte.data.modality import Modality
 from forte.data.ontology.core import Grid
+from ft.onto.base_ontology import ImagePayload
 import numpy as np
 
 from numpy import array_equal
@@ -31,62 +33,80 @@ class ImageAnnotationTest(unittest.TestCase):
     """
 
     def setUp(self):
-        self.datapack = DataPack("image")
+        self.datapack1 = DataPack("image")
         self.line = np.zeros((6, 12))
         self.line[2, 2] = 1
         self.line[3, 3] = 1
         self.line[4, 4] = 1
-        ip = ImagePayload(self.datapack, 0)
+        ip = ImagePayload(self.datapack1, 0)
         ip.set_cache(self.line)
-        ImageAnnotation(self.datapack)
+
+        self.datapack2 = DataPack("image")
+        empty_img = np.zeros((641, 481))
+        ip = ImagePayload(self.datapack2, 0)
+        ip.set_cache(empty_img)
 
     def test_image_annotation(self):
+        self.line = np.zeros((6, 12))
+        self.line[2, 2] = 1
+        self.line[3, 3] = 1
+        self.line[4, 4] = 1
+        ip = ImagePayload(self.datapack1, 0)
+        ip.set_cache(self.line)
+        ImageAnnotation(self.datapack1)
         self.assertEqual(
-            self.datapack.get_single(ImageAnnotation).image_payload_idx, 0
+            self.datapack1.get_single(ImageAnnotation).image_payload_idx, 0
         )
 
         self.assertTrue(
             array_equal(
-                self.datapack.get_payload_at(Modality.Image, 0).cache, self.line
+                self.datapack1.get_payload_at(Modality.Image, 0).cache,
+                self.line,
             )
         )
-        new_pack = DataPack.from_string(self.datapack.to_string())
+        new_pack = DataPack.from_string(self.datapack1.to_string())
         self.assertEqual(
-            new_pack.audio_annotations, self.datapack.audio_annotations
+            new_pack.audio_annotations, self.datapack1.audio_annotations
         )
 
     def test_wrong_box(self):
+        empty_img = np.zeros((641, 481))
+        ip = ImagePayload(self.datapack2, 0)
+        ip.set_cache(empty_img)
         # test a box has a wrong shape (negative height)
         def fn():
-            Box(self.datapack, -1, 1)
+            Box(self.datapack2, -1, 1)
 
         self.assertRaises(ValueError, fn)
 
         # test a box has a wrong center (negative center y coordinate)
         def fn():
-            Box(self.datapack, 1, 1, -1, 1)
+            Box(self.datapack2, 1, 1, -1, 1)
 
         self.assertRaises(ValueError, fn)
 
         # test a box has a wrong center (not large enough to contain the box)
         def fn():
-            Box(self.datapack, 4, 4, 1, 1)
+            Box(self.datapack2, 4, 4, 1, 1)
 
         self.assertRaises(ValueError, fn)
 
-        # TODO: test a box has a wrong center (too large that box is out of the image)
-        # suppose the image is 4x4
-        # def fn():
-        #     # valid box
-        #     Box(self.datapack, 4, 4, 3, 3)
-        # self.assertRaises(ValueError, fn)
+        # test a box that is too large
+        def fn():
+            Box(self.datapack2, 641, 481, 3, 3)
 
-        # TODO: test a box has a wrong offset
+        self.assertRaises(ValueError, fn)
+
+        # test a box that is out of the image
+        def fn():
+            Box(self.datapack2, 30, 30, 630, 470)
+
+        self.assertRaises(ValueError, fn)
 
     def test_incomplete_box(self):
         # test a box doesn't have a center
         def fn():
-            b = Box(self.datapack, 1, 1)
+            b = Box(self.datapack2, 1, 1)
             b.corners
 
         self.assertRaises(ValueError, fn)
@@ -94,7 +114,8 @@ class ImageAnnotationTest(unittest.TestCase):
     def test_simple_box(self):
         # test a box not associated with any grid
         # test its propertys are correct
-        b = Box(self.datapack, 640, 480, 320, 240)
+
+        b = Box(self.datapack2, 640, 480, 320, 240)
         self.assertEqual(b.cx, 240)
         self.assertEqual(b.cy, 320)
         self.assertFalse(b.is_grid_associated)
@@ -109,7 +130,10 @@ class ImageAnnotationTest(unittest.TestCase):
     def test_ground_truth_box(self):
         # test a box shape and absolute position is given
         # we can compute the relative position to a grid cell
-        b = Box(self.datapack, 640, 480, 320, 240)
+        empty_img = np.zeros((641, 481))
+        ip = ImagePayload(self.datapack2, 0)
+        ip.set_cache(empty_img)
+        b = Box(self.datapack2, 640, 480, 320, 240)
         g = Grid(64, 48, 640, 480)  # each grid cell has a shape of 10 x 10
         # to simplify the test, we use the Box has the same shape as the image
         self.assertFalse(b.is_grid_associated)
@@ -125,7 +149,10 @@ class ImageAnnotationTest(unittest.TestCase):
     def test_predicted_box(self):
         # test a box has a predicted shape and offset given a grid cell
         # we can compute the absolute position
-        b = Box(self.datapack, 640, 480)
+        empty_img = np.zeros((641, 481))
+        ip = ImagePayload(self.datapack2, 0)
+        ip.set_cache(empty_img)
+        b = Box(self.datapack2, 640, 480)
         g = Grid(64, 48, 640, 480)  # each grid cell has a shape of 10 x 10
         # to simplify the test, we use the Box has the same shape as the image
         self.assertFalse(b.is_grid_associated)
