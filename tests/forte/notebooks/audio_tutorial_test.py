6c146da459fc443c73a04ced144e5ab9657b3a84
Bhaskar Rao <bhaskarrao@Bhaskar.local>
2022-08-12 10:06:06 +0400

    test case 2

--- tests/forte/notebooks/audio_tutorial_test.py
+++ tests/forte/notebooks/audio_tutorial_test.py
@@ -2,7 +2,7 @@ from testbook import testbook
 
 
 @testbook(
-    "docs/notebook_tutorial/Automatic_Speech_Recognition.ipynb", execute=False
+    "docs/notebook_tutorial/Automatic_Speech_Recognition.ipynb", execute=True
 )
 def test_text_classification_pipeline(tb):
     # if we just want to run through the notebook
