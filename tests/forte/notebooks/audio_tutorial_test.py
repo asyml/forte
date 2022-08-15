939c9ec056685487cf6cc37c85cef6857fa6d98f
bhaskar2443053 <bhaskar2443053@gmail.com>
2022-08-15 13:56:51 +0400

    add test

--- tests/forte/notebooks/audio_tutorial_test.py
+++ tests/forte/notebooks/audio_tutorial_test.py
@@ -1,25 +1,21 @@
 from testbook import testbook
-import os
 
 @testbook(
-    "docs/notebook_tutorial/Automatic_Speech_Recognition.ipynb", execute=False
+    "docs/notebook_tutorial/Automatic_Speech_Recognition.ipynb",
+    execute=False
 )
 def test_Automatic_Speech_Recognition(tb):
     # input file
     tb.execute_cell("input_file")
-    #install
-<<<<<<< HEAD
-    tb.execute_cell("install")
-=======
-    #tb.execute_cell("install")
->>>>>>> cluster_test_notebook
+    # install
+    # tb.execute_cell("install")
     # imports_1
     tb.execute_cell("imports_1")
-    #processor 1
+    # processor 1
     tb.execute_cell("SpeakerSegmentation")
-    
     tb.execute_cell("pipeline1")
     tb.execute_cell("import_2")
     tb.execute_cell("AudioUtterance")
     tb.execute_cell("pipeline2")
     tb.execute_cell("inference")
+    
