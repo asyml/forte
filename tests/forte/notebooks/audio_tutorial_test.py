5e60bef973553a2602e67000998b1547fec528dd
Bhaskar Rao <bhaskarrao@Bhaskar.local>
2022-08-15 11:24:26 +0400

    test day2

--- tests/forte/notebooks/audio_tutorial_test.py
+++ tests/forte/notebooks/audio_tutorial_test.py
@@ -1,9 +1,20 @@
 from testbook import testbook
-
+import os
 
 @testbook(
-    "docs/notebook_tutorial/Automatic_Speech_Recognition.ipynb", execute=True
+    "docs/notebook_tutorial/Automatic_Speech_Recognition.ipynb", execute=False
 )
-def test_text_classification_pipeline(tb):
-    # if we just want to run through the notebook
-    pass
+def test_Automatic_Speech_Recognition(tb):
+    # input file
+    tb.execute_cell("input_file")
+    # imports_1
+    tb.execute_cell("imports_1")
+    #processor 1
+    tb.execute_cell("SpeakerSegmentation")
+    
+    tb.execute_cell("pipeline1")
+    tb.execute_cell("import_2")
+    # test Article ontology
+    tb.execute_cell("AudioUtterance")
+    tb.execute_cell("pipeline2")
+    tb.execute_cell("inference")
