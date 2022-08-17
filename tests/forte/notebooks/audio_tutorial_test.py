2636fa275f2ee160de2111d6e5c8716e65d5adf3
bhaskar2443053 <bhaskar2443053@gmail.com>
2022-08-17 11:50:55 +0400

    env skip

--- tests/forte/notebooks/audio_tutorial_test.py
+++ tests/forte/notebooks/audio_tutorial_test.py
@@ -1,5 +1,6 @@
 from testbook import testbook
-import os
+import sys
+
 
 @testbook(
     "docs/notebook_tutorial/Automatic_Speech_Recognition.ipynb",
@@ -7,21 +8,22 @@ import os
 )
 
 
-env = os.environ.get('ENVIRONMENT', 'test')
-print(env)
-def test_Automatic_Speech_Recognition(tb):
 
+def test_Automatic_Speech_Recognition(tb):
+    if sys.version_info[0] > 3.7:
     # input file
-    tb.execute_cell("input_file")
-    # install
-    tb.execute_cell("install")
-    tb.execute_cell("install2")
-    # imports_1
-    tb.execute_cell("imports_1")
-    # processor 1
-    tb.execute_cell("SpeakerSegmentation")
-    tb.execute_cell("pipeline1")
-    tb.execute_cell("import_2")
-    tb.execute_cell("AudioUtterance")
-    tb.execute_cell("pipeline2")
-    tb.execute_cell("inference")
+        tb.execute_cell("input_file")
+        # install
+        tb.execute_cell("install")
+        tb.execute_cell("install2")
+        # imports_1
+        tb.execute_cell("imports_1")
+        # processor 1
+        tb.execute_cell("SpeakerSegmentation")
+        tb.execute_cell("pipeline1")
+        tb.execute_cell("import_2")
+        tb.execute_cell("AudioUtterance")
+        tb.execute_cell("pipeline2")
+        tb.execute_cell("inference")
+    else:
+        tb.execute_cell("input_file")
